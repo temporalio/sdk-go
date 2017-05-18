738d8c3032748ad5ac4a6466a8e023780dc59e32
yiminc <yiminc@users.noreply.github.com>
2017-05-18 13:40:39 -0700

    is decision event (#111)
    * is decision event
    
    * use one case with all events

--- internal_task_handlers.go
+++ internal_task_handlers.go
@@ -139,9 +139,15 @@ func (eh *history) IsNextDecisionFailed() bool {
 
 func isDecisionEvent(eventType s.EventType) bool {
 	switch eventType {
-	case s.EventType_WorkflowExecutionCompleted, s.EventType_WorkflowExecutionFailed, s.EventType_WorkflowExecutionTimedOut:
-		return true
-	case s.EventType_ActivityTaskScheduled, s.EventType_TimerStarted:
+	case s.EventType_WorkflowExecutionCompleted,
+		s.EventType_WorkflowExecutionFailed,
+		s.EventType_WorkflowExecutionCanceled,
+		s.EventType_ActivityTaskScheduled,
+		s.EventType_ActivityTaskCancelRequested,
+		s.EventType_TimerStarted,
+		s.EventType_TimerCanceled,
+		s.EventType_MarkerRecorded,
+		s.EventType_RequestCancelExternalWorkflowExecutionInitiated:
 		return true
 	default:
 		return false
@@ -328,7 +334,7 @@ ProcessEvents:
 		isInReplay := reorderedEvents[0].GetEventId() < reorderedHistory.LastNonReplayedID()
 		for i, event := range reorderedEvents {
 			isLast := !isInReplay && i == len(reorderedEvents)-1
-			if isEventTypeRespondToDecision(event.GetEventType()) {
+			if isDecisionEvent(event.GetEventType()) {
 				respondEvents = append(respondEvents, event)
 			}
 
@@ -398,32 +404,6 @@ ProcessEvents:
 		stackTrace = eventHandler.StackTrace()
 	}
 	return taskCompletionRequest, stackTrace, nil
-}
-
-// for every decision, there is one EventType respond to that decision.
-func isEventTypeRespondToDecision(t s.EventType) bool {
-	switch t {
-	case s.EventType_ActivityTaskScheduled:
-		return true
-	case s.EventType_ActivityTaskCancelRequested:
-		return true
-	case s.EventType_TimerStarted:
-		return true
-	case s.EventType_TimerCanceled:
-		return true
-	case s.EventType_WorkflowExecutionCompleted:
-		return true
-	case s.EventType_WorkflowExecutionFailed:
-		return true
-	case s.EventType_MarkerRecorded:
-		return true
-	case s.EventType_RequestCancelExternalWorkflowExecutionInitiated:
-		return true
-	case s.EventType_WorkflowExecutionCanceled:
-		return true
-	default:
-		return false
-	}
 }
 
 func matchReplayWithHistory(replayDecisions []*s.Decision, historyEvents []*s.HistoryEvent) error {
