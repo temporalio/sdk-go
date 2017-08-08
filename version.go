3690dcd6866f0e5983ba30e37f1647cfbdc3cbb5
yiminc <yiminc@users.noreply.github.com>
2017-08-08 15:30:42 -0700

    bump version to 0.3.1 (#211)

--- version.go
+++ version.go
@@ -29,4 +29,4 @@ package cadence
 // Update to this version number is typically done
 // by the cadence team as part of a major feature or
 // behavior change
-const LibraryVersion = "v0.3.0"
+const LibraryVersion = "v0.3.1"
