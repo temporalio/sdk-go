8f3c241553281f11f7df2e088948aa065d8af458
Bowei Xu <boweixu@uber.com>
2018-05-25 14:49:35 -0700

    Release verison 0.7.0 (#485)

--- internal/version.go
+++ internal/version.go
@@ -35,7 +35,7 @@ package internal
 // that are writing workflows. So every time we change API
 // that can affect them we have to change this number.
 // Format: MAJOR.MINOR.PATCH
-const LibraryVersion = "0.6.1"
+const LibraryVersion = "0.7.0"
 
 // FeatureVersion is a semver that represents the
 // feature set of this cadence client library support.
