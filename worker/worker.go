d9b2a201a90d565f77c2ed3d4076cb7a3482528e
Dustin Metzgar <dmetzgar@users.noreply.github.com>
2019-05-21 12:23:21 -0700

    Fix doc of worker.New
    Doc doesn't look right (https://godoc.org/go.uber.org/cadence/worker#New). Godoc doesn't support bullets but we can use a code block instead.

--- worker/worker.go
+++ worker/worker.go
@@ -59,11 +59,12 @@ const (
 )
 
 // New creates an instance of worker for managing workflow and activity executions.
-// service 	- thrift connection to the cadence server.
-// domain - the name of the cadence domain.
-// taskList 	- is the task list name you use to identify your client worker, also
-// 		  identifies group of workflow and activity implementations that are hosted by a single worker process.
-// options 	-  configure any worker specific options like logger, metrics, identity.
+//    service  - thrift connection to the cadence server
+//    domain   - the name of the cadence domain
+//    taskList - is the task list name you use to identify your client worker, also
+//               identifies group of workflow and activity implementations that are 
+//               hosted by a single worker process
+//    options  - configure any worker specific options like logger, metrics, identity
 func New(
 	service workflowserviceclient.Interface,
 	domain string,
