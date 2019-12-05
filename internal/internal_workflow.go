7f500041153af7bf23ba674748d28626b2191172
Liang Mei <meiliang@uber.com>
2019-12-05 14:22:40 -0800

    Revert "Change ExecuteUntilAllBlocked to return an error, rather than a pointer (#905)"
    This reverts commit 307b1ac629dd0d12189debe420fe3e3e4d7ab3fb.

--- internal/internal_workflow.go
+++ internal/internal_workflow.go
@@ -81,7 +81,7 @@ type (
 	dispatcher interface {
 		// ExecuteUntilAllBlocked executes coroutines one by one in deterministic order
 		// until all of them are completed or blocked on Channel or Selector
-		ExecuteUntilAllBlocked() (err error)
+		ExecuteUntilAllBlocked() (err *workflowPanicError)
 		// IsDone returns true when all of coroutines are completed
 		IsDone() bool
 		Close()             // Destroys all coroutines without waiting for their completion
@@ -849,7 +849,7 @@ func (d *dispatcherImpl) newState(name string) *coroutineState {
 	return c
 }
 
-func (d *dispatcherImpl) ExecuteUntilAllBlocked() (err error) {
+func (d *dispatcherImpl) ExecuteUntilAllBlocked() (err *workflowPanicError) {
 	d.mutex.Lock()
 	if d.closed {
 		panic("dispatcher is closed")
