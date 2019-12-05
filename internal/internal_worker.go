a67d04578899bcddb579f43b08d21ab32b0400ff
Steven L <imgroxx@gmail.com>
2019-12-05 13:46:21 -0800

    Fix binary checksum race condition and lack of err handling (#900)
    In practice this is probably only an issue in tests, as other use of the checksum
    depends on a running worker which would have initialized the value.
    Still, if it's un-protected, Go has no obligation to propagate the change, so it's
    risky to leave.
    
    This now also detects and panics on errs from lazily initializing the checksum,
    and explicitly ignores the Close() error (to keep errcheck happy, if used) since
    it's relatively safe to do so.

--- internal/internal_worker.go
+++ internal/internal_worker.go
@@ -1042,6 +1042,7 @@ var binaryChecksumLock sync.Mutex
 func SetBinaryChecksum(checksum string) {
 	binaryChecksumLock.Lock()
 	defer binaryChecksumLock.Unlock()
+
 	binaryChecksum = checksum
 }
 
@@ -1049,6 +1050,11 @@ func initBinaryChecksum() error {
 	binaryChecksumLock.Lock()
 	defer binaryChecksumLock.Unlock()
 
+	return initBinaryChecksumLocked()
+}
+
+// callers MUST hold binaryChecksumLock before calling
+func initBinaryChecksumLocked() error {
 	if len(binaryChecksum) > 0 {
 		return nil
 	}
@@ -1062,7 +1068,9 @@ func initBinaryChecksum() error {
 	if err != nil {
 		return err
 	}
-	defer f.Close()
+	defer func() {
+		_ = f.Close() // error is unimportant as it is read-only
+	}()
 
 	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
@@ -1076,8 +1084,14 @@ func initBinaryChecksum() error {
 }
 
 func getBinaryChecksum() string {
+	binaryChecksumLock.Lock()
+	defer binaryChecksumLock.Unlock()
+
 	if len(binaryChecksum) == 0 {
-		initBinaryChecksum()
+		err := initBinaryChecksumLocked()
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	return binaryChecksum
