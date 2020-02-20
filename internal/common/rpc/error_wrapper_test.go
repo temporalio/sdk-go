54c14af3ff393ef875fc82f9684d2d894e74b6e8
Alex Shtin <alex@shtin.com>
2020-02-19 18:22:06 -0800

    Unit test for error wrapper.

---
+++ internal/common/rpc/error_wrapper_test.go
@@ -0,0 +1,41 @@
+package rpc
+
+import (
+	"testing"
+
+	"github.com/gogo/status"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"go.temporal.io/temporal-proto/errordetails"
+	"go.temporal.io/temporal-proto/serviceerror"
+	"go.temporal.io/temporal-proto/workflowservicemock"
+	"google.golang.org/grpc/codes"
+)
+
+func TestErrorWrapper_NotFound(t *testing.T) {
+	require := require.New(t)
+	wrapper := NewWorkflowServiceErrorWrapper(workflowservicemock.NewMockWorkflowServiceClient(gomock.NewController(t)))
+
+	st := status.Error(codes.NotFound, "Something not found")
+
+	svcerr := wrapper.convertError(st)
+	require.IsType(&serviceerror.NotFound{}, svcerr)
+	require.Equal("Something not found", svcerr.Error())
+}
+
+func TestErrorWrapper_WorkflowExecutionAlreadyStarted(t *testing.T) {
+	require := require.New(t)
+	wrapper := NewWorkflowServiceErrorWrapper(workflowservicemock.NewMockWorkflowServiceClient(gomock.NewController(t)))
+
+	st, _ := status.New(codes.AlreadyExists, "Something started").WithDetails(&errordetails.WorkflowExecutionAlreadyStartedFailure{
+		StartRequestId: "srId",
+		RunId:          "rId",
+	})
+
+	svcerr := wrapper.convertError(st.Err())
+	require.IsType(&serviceerror.WorkflowExecutionAlreadyStarted{}, svcerr)
+	require.Equal("Something started", svcerr.Error())
+	weasErr := svcerr.(*serviceerror.WorkflowExecutionAlreadyStarted)
+	require.Equal("rId", weasErr.RunId)
+	require.Equal("srId", weasErr.StartRequestId)
+}
