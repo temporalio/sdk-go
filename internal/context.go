7e919418a7ac1f8d7489b4adf9d91a841151ddbe
Alex Shtin <alex@shtin.com>
2020-03-27 13:32:10 -0700

    Use correct context package.

--- internal/context.go
+++ internal/context.go
@@ -103,7 +103,7 @@ type Context interface {
 	// 	// Package user defines a User type that's stored in Contexts.
 	// 	package user
 	//
-	// 	import "golang.org/x/net/context"
+	// 	import "context"
 	//
 	// 	// User is the type of value stored in the Contexts.
 	// 	type User struct {...}
