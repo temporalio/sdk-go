141f0006a49634a7b8f60d9e2e329a076d0d83b2
Alex Shtin <alex@shtin.com>
2020-04-17 09:42:32 -0700

    Fix comment.

--- internal/encoded.go
+++ internal/encoded.go
@@ -62,14 +62,12 @@ type (
 
 	// DataConverter is used by the framework to serialize/deserialize input and output of activity/workflow
 	// that need to be sent over the wire.
-	// To encode/decode workflow arguments, one should set DataConverter in two places:
-	//   1. Workflow worker, through worker.Options
-	//   2. Client, through client.Options
+	// To encode/decode workflow arguments, one should set DataConverter in for Client, through client.Options.
 	// To encode/decode Activity/ChildWorkflow arguments, one should set DataConverter in two places:
 	//   1. Inside workflow code, use workflow.WithDataConverter to create new Context,
 	// and pass that context to ExecuteActivity/ExecuteChildWorkflow calls.
 	// Temporal support using different DataConverters for different activity/childWorkflow in same workflow.
-	//   2. Activity/Workflow worker that run these activity/childWorkflow, through worker.Options.
+	//   2. Activity/Workflow worker that run these activity/childWorkflow, through cleint.Options.
 	DataConverter interface {
 		// ToData implements conversion of a list of values.
 		ToData(value ...interface{}) (*commonpb.Payload, error)
