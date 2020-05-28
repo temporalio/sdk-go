9cf24e995adb5f976bd219ccdf8fe69f5af14cdd
Alex Shtin <alex@shtin.com>
2020-05-28 12:13:59 -0700

    Address feedback.

--- error.go
+++ error.go
@@ -40,10 +40,10 @@ and actual error which caused activity failure. This internal error can be unwra
 Below are the possible types of internal error:
 1) *ApplicationError: (this should be the most common one)
 	*ApplicationError can be returned in two cases:
-		- If activity implementation returns *ApplicationError by using NewApplicationError() API.
-		  The err would contain a message, details, and NonRetryable flag. Workflow code could check this flag and details to determine
-		  what kind of error it was and take actions based on it. The details is encoded payload which workflow code could extract
-		  to strong typed variable. Workflow code needs to know what the types of the encoded details are before extracting them.
+		- If activity implementation returns *ApplicationError by using NewRetryableApplicationError()/NewNonRetryableApplicationError() API.
+		  The error would contain a message and optional details. Workflow code could extract details to string typed variable, determine
+		  what kind of error it was, and take actions based on it. The details is encoded payload therefore, workflow code needs to know what
+          the types of the encoded details are before extracting them.
 		- If activity implementation returns errors other than from NewApplicationError() API. In this case GetOriginalType()
 		  will return orginal type of an error represented as string. Workflow code could check this type to determine what kind of error it was
 		  and take actions based on the type. These errors are retryable by default, unless error type is specified in retry policy.
@@ -58,16 +58,18 @@ Below are the possible types of internal error:
 	The SDK will present that failure as *PanicError. The err contains a string	representation of the panic message and
 	the call stack when panic was happen.
 Workflow code could handle errors based on different types of error. Below is sample code of how error handling looks like.
+
 err := workflow.ExecuteActivity(ctx, MyActivity, ...).Get(ctx, nil)
 if err != nil {
 	var applicationErr *ApplicationError
 	if errors.As(err, &applicationError) {
+		// retrieve error message
+		fmt.Println(applicationError.Error())
+
 		// handle activity errors (created via NewApplicationError() API)
-		if !applicationErr.NonRetryable() {
-			// manually retry activity
-		}
 		var detailMsg string // assuming activity return error by NewApplicationError("message", true, "string details")
 		applicationErr.Details(&detailMsg) // extract strong typed details
+
 		// handle activity errors (errors created other than using NewApplicationError() API)
 		switch err.OriginalType() {
 		case "CustomErrTypeA":
@@ -78,10 +80,12 @@ if err != nil {
 			// newer version of activity could return new errors that workflow was not aware of.
 		}
 	}
+
 	var canceledErr *CanceledError
 	if errors.As(err, &canceledErr) {
 		// handle cancellation
 	}
+
 	var timeoutErr *TimeoutError
 	if errors.As(err, &timeoutErr) {
 		// handle timeout, could check timeout type by timeoutErr.TimeoutType()
@@ -95,13 +99,16 @@ if err != nil {
         default:
         }
 	}
+
 	var panicErr *PanicError
 	if errors.As(err, &panicErr) {
 		// handle panic, message and stack trace are available by panicErr.Error() and panicErr.StackTrace()
 	}
 }
 Errors from child workflow should be handled in a similar way, except that instance of *ChildWorkflowExecutionError is returned to
-workflow code. It will contains *ActivityTaskError, which in turn will contains on of the errors above.
+workflow code. It might contain *ActivityTaskError in case if error comes from activity (which in turn will contain on of the errors above),
+or *ApplicationError in case if error comes from child workflow itslef.
+
 When panic happen in workflow implementation code, SDK catches that panic and causing the decision timeout.
 That decision task will be retried at a later time (with exponential backoff retry intervals).
 Workflow consumers will get an instance of *WorkflowExecutionError. This error will contains one of errors above.
@@ -146,10 +153,16 @@ type (
 // ErrNoData is returned when trying to extract strong typed data while there is no data available.
 var ErrNoData = internal.ErrNoData
 
-// NewApplicationError create new instance of *ApplicationError with reason and optional details.
+// NewNonRetryableApplicationError creates new instance of non-retryable *ApplicationError with reason and optional details.
 // Use ApplicationError for any use case specific errors that cross activity and child workflow boundaries.
-func NewApplicationError(reason string, nonRetryable bool, details ...interface{}) *ApplicationError {
-	return internal.NewApplicationError(reason, nonRetryable, details...)
+func NewNonRetryableApplicationError(reason string, details ...interface{}) *ApplicationError {
+	return internal.NewApplicationError(reason, true, details...)
+}
+
+// NewRetryableApplicationError creates new instance of retryable *ApplicationError with reason and optional details.
+// Use ApplicationError for any use case specific errors that cross activity and child workflow boundaries.
+func NewRetryableApplicationError(reason string, details ...interface{}) *ApplicationError {
+	return internal.NewApplicationError(reason, false, details...)
 }
 
 // NewCanceledError creates CanceledError instance.
