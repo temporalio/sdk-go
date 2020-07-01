70c630c4b5568371569c7e112c5d9797c23bff17
Alex Shtin <alex@shtin.com>
2020-06-30 19:50:29 -0700

    Add errType to non-retryable error.

--- error.go
+++ error.go
@@ -166,10 +166,10 @@ func NewApplicationErrorWithCause(message, errType string, cause error, details 
 	return internal.NewApplicationError(message, errType, false, cause, details...)
 }
 
-// NewNonRetryableApplicationError creates new instance of non-retryable *ApplicationError with message, and optional cause and details.
+// NewNonRetryableApplicationError creates new instance of non-retryable *ApplicationError with message, type, and optional cause and details.
 // Use ApplicationError for any use case specific errors that cross activity and child workflow boundaries.
-func NewNonRetryableApplicationError(message string, cause error, details ...interface{}) *ApplicationError {
-	return internal.NewApplicationError(message, "", true, cause, details...)
+func NewNonRetryableApplicationError(message, errType string, cause error, details ...interface{}) *ApplicationError {
+	return internal.NewApplicationError(message, errType, true, cause, details...)
 }
 
 // NewCanceledError creates CanceledError instance.
