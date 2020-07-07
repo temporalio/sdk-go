uber-go/cadence-client

Improve session framework heartbeat mechanism (#965)

internal/internal_task_handlers_test.go
@@ -1171,7 +1171,7 @@ func (t *TaskHandlersTestSuite) TestHeartBeat_NoError() {
 		taskToken: nil,
 	}
 
-	heartbeatErr := cadenceInvoker.Heartbeat(nil)
+	heartbeatErr := cadenceInvoker.Heartbeat(nil, false)
 
 	t.Nil(heartbeatErr)
 }
@@ -1191,7 +1191,7 @@ func (t *TaskHandlersTestSuite) TestHeartBeat_NilResponseWithError() {
 		0,
 		make(chan struct{}))
 
-	heartbeatErr := cadenceInvoker.Heartbeat(nil)
+	heartbeatErr := cadenceInvoker.Heartbeat(nil, false)
 	t.NotNil(heartbeatErr)
 	_, ok := (heartbeatErr).(*s.EntityNotExistsError)
 	t.True(ok, "heartbeatErr must be EntityNotExistsError.")
@@ -1215,7 +1215,7 @@ func (t *TaskHandlersTestSuite) TestHeartBeat_NilResponseWithDomainNotActiveErro
 		0,
 		make(chan struct{}))
 
-	heartbeatErr := cadenceInvoker.Heartbeat(nil)
+	heartbeatErr := cadenceInvoker.Heartbeat(nil, false)
 	t.NotNil(heartbeatErr)
 	_, ok := (heartbeatErr).(*s.DomainNotActiveError)
 	t.True(ok, "heartbeatErr must be DomainNotActiveError.")

temporalio/sdk-go

 Conflicts:
	internal/activity.go
	internal/internal_task_handlers.go
	internal/internal_task_handlers_test.go
	internal/session.go

internal/internal_task_handlers_test.go
@@ -1312,7 +1312,7 @@ func (t *TaskHandlersTestSuite) TestHeartBeat_NoError() {
 		taskToken: nil,
 	}
 
-	heartbeatErr := temporalInvoker.Heartbeat(nil)
+	heartbeatErr := temporalInvoker.Heartbeat(nil, false)
 
 	t.NoError(heartbeatErr)
 }
@@ -1331,7 +1331,7 @@ func (t *TaskHandlersTestSuite) TestHeartBeat_NilResponseWithError() {
 		0,
 		make(chan struct{}))
 
-	heartbeatErr := temporalInvoker.Heartbeat(nil)
+	heartbeatErr := temporalInvoker.Heartbeat(nil, false)
 	t.NotNil(heartbeatErr)
 	t.IsType(&serviceerror.NotFound{}, heartbeatErr, "heartbeatErr must be of type NotFound.")
 }
@@ -1353,7 +1353,7 @@ func (t *TaskHandlersTestSuite) TestHeartBeat_NilResponseWithNamespaceNotActiveE
 		0,
 		make(chan struct{}))
 
-	heartbeatErr := temporalInvoker.Heartbeat(nil)
+	heartbeatErr := temporalInvoker.Heartbeat(nil, false)
 	t.NotNil(heartbeatErr)
 	t.IsType(&serviceerror.NamespaceNotActive{}, heartbeatErr, "heartbeatErr must be of type NamespaceNotActive.")
 	t.True(called)