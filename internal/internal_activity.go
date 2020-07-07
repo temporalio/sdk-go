uber-go/cadence-client

Add Context Propagation support for Local Activities (#986)

internal/internal_activity.go
@@ -88,6 +88,7 @@ type (
 		DataConverter DataConverter
 		Attempt       int32
 		ScheduledTime time.Time
+		Header        *shared.Header
 	}
 
 	// asyncActivityClient for requesting activity execution

internal/internal_event_handlers.go
@@ -132,6 +132,7 @@ type (
 		attempt      int32 // attempt starting from 0
 		retryPolicy  *RetryPolicy
 		expireTime   time.Time
+		header       *shared.Header
 	}
 
 	localActivityMarkerData struct {
@@ -503,6 +504,7 @@ func newLocalActivityTask(params executeLocalActivityParams, callback laResultHa
 		callback:    callback,
 		retryPolicy: params.RetryPolicy,
 		attempt:     params.Attempt,
+		header:      params.Header,
 	}
 
 	if params.RetryPolicy != nil && params.RetryPolicy.ExpirationInterval > 0 {

test/workflow_test.go
@@ -511,9 +511,21 @@ func (w *Workflows) InspectLocalActivityInfo(ctx workflow.Context) error {
 	wfType := info.WorkflowType.Name
 	taskList := info.TaskListName
 	ctx = workflow.WithLocalActivityOptions(ctx, w.defaultLocalActivityOptions())
-	activites := Activities{}
+	activities := Activities{}
 	return workflow.ExecuteLocalActivity(
-		ctx, activites.InspectActivityInfo, domain, taskList, wfType).Get(ctx, nil)
+		ctx, activities.InspectActivityInfo, domain, taskList, wfType).Get(ctx, nil)
+}
+
+func (w *Workflows) WorkflowWithLocalActivityCtxPropagation(ctx workflow.Context) (string, error) {
+	ctx = workflow.WithLocalActivityOptions(ctx, w.defaultLocalActivityOptions())
+	ctx = workflow.WithValue(ctx, contextKey(testContextKey), "test-data-in-context")
+	activities := Activities{}
+	var result string
+	err := workflow.ExecuteLocalActivity(ctx, activities.DuplicateStringInContext).Get(ctx, &result)
+	if err != nil {
+		return "", err
+	}
+	return result, nil
 }
 
 func (w *Workflows) register(worker worker.Worker) {
@@ -541,6 +553,7 @@ func (w *Workflows) register(worker worker.Worker) {
 	worker.RegisterWorkflow(w.LargeQueryResultWorkflow)
 	worker.RegisterWorkflow(w.RetryTimeoutStableErrorWorkflow)
 	worker.RegisterWorkflow(w.ConsistentQueryWorkflow)
+	worker.RegisterWorkflow(w.WorkflowWithLocalActivityCtxPropagation)
 
 }
 

temporalio/sdk-go

internal/internal_activity.go
@@ -97,6 +97,7 @@ type (
 		DataConverter DataConverter
 		Attempt       int32
 		ScheduledTime time.Time
+		Header        *commonpb.Header
 	}
 
 	// AsyncActivityClient for requesting activity execution

internal/internal_event_handlers.go
@@ -140,6 +140,7 @@ type (
 		attempt      int32 // attempt starting from 0
 		retryPolicy  *RetryPolicy
 		expireTime   time.Time
+		header       *commonpb.Header
 	}
 
 	localActivityMarkerData struct {
@@ -515,6 +516,7 @@ func newLocalActivityTask(params ExecuteLocalActivityParams, callback LocalActiv
 		callback:    callback,
 		retryPolicy: params.RetryPolicy,
 		attempt:     params.Attempt,
+		header:      params.Header,
 	}
 
 	if params.ScheduleToCloseTimeoutSeconds > 0 {

test/workflow_test.go
@@ -603,9 +603,21 @@ func (w *Workflows) InspectLocalActivityInfo(ctx workflow.Context) error {
 	wfType := info.WorkflowType.Name
 	taskQueue := info.TaskQueueName
 	ctx = workflow.WithLocalActivityOptions(ctx, w.defaultLocalActivityOptions())
-	activites := Activities{}
+	activities := Activities{}
 	return workflow.ExecuteLocalActivity(
-		ctx, activites.InspectActivityInfo, namespace, taskQueue, wfType).Get(ctx, nil)
+		ctx, activities.InspectActivityInfo, namespace, taskQueue, wfType).Get(ctx, nil)
+}
+
+func (w *Workflows) WorkflowWithLocalActivityCtxPropagation(ctx workflow.Context) (string, error) {
+	ctx = workflow.WithLocalActivityOptions(ctx, w.defaultLocalActivityOptions())
+	ctx = workflow.WithValue(ctx, contextKey(testContextKey), "test-data-in-context")
+	activities := Activities{}
+	var result string
+	err := workflow.ExecuteLocalActivity(ctx, activities.DuplicateStringInContext).Get(ctx, &result)
+	if err != nil {
+		return "", err
+	}
+	return result, nil
 }
 
 func (w *Workflows) BasicSession(ctx workflow.Context) ([]string, error) {
@@ -661,6 +673,7 @@ func (w *Workflows) register(worker worker.Worker) {
 	worker.RegisterWorkflow(w.RetryTimeoutStableErrorWorkflow)
 	worker.RegisterWorkflow(w.ConsistentQueryWorkflow)
 	worker.RegisterWorkflow(w.BasicSession)
+	worker.RegisterWorkflow(w.WorkflowWithLocalActivityCtxPropagation)
 }
 
 func (w *Workflows) defaultActivityOptions() workflow.ActivityOptions {