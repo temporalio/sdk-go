2245a3098a2594bc2c5d07caedbd8cd2c4f71dae
Vitaly <Vitaly.Arbuzov@gmail.com>
2021-02-23 16:20:50 -0800

    Adding support for custom gRPC headers (#368)

--- internal/grpc_dialer.go
+++ internal/grpc_dialer.go
@@ -34,6 +34,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 
 	"go.temporal.io/sdk/internal/common/metrics"
 )
@@ -86,8 +87,25 @@ func dial(params dialParameters) (*grpc.ClientConn, error) {
 	)
 }
 
-func requiredInterceptors(metricScope tally.Scope) []grpc.UnaryClientInterceptor {
-	return []grpc.UnaryClientInterceptor{metrics.NewScopeInterceptor(metricScope), errorInterceptor}
+func requiredInterceptors(metricScope tally.Scope, headersProvider HeadersProvider) []grpc.UnaryClientInterceptor {
+	interceptors := []grpc.UnaryClientInterceptor{metrics.NewScopeInterceptor(metricScope), errorInterceptor}
+	if headersProvider != nil {
+		interceptors = append(interceptors, headersProviderInterceptor(headersProvider))
+	}
+	return interceptors
+}
+
+func headersProviderInterceptor(headersProvider HeadersProvider) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		headers, err := headersProvider.GetHeaders(ctx)
+		if err != nil {
+			return err
+		}
+		for k, v := range headers {
+			ctx = metadata.AppendToOutgoingContext(ctx, k, v)
+		}
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
 }
 
 func errorInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
