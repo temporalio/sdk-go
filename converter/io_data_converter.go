59d103b68757feea540306901820bc1c5d415e37
Chad Retz <chad@temporal.io>
2021-10-21 14:47:46 -0500

    Add generic IO converter with initial Zlib implementation
    Fixes #596

---
+++ converter/io_data_converter.go
@@ -0,0 +1,232 @@
+package converter
+
+import (
+	"bytes"
+	"compress/zlib"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	"github.com/golang/protobuf/proto"
+	"go.temporal.io/api/common/v1"
+	commonpb "go.temporal.io/api/common/v1"
+)
+
+// IODataConverterOptions are options for NewIODataConverter.
+type IODataConverterOptions struct {
+
+	// Algorithm is the algorithm for raw IO conversation. All fields must be set.
+	// ZlibAlgorithm can be used for compression.
+	Algorithm IODataConverterAlgorithm
+
+	// If this is greater than zero, this converter will not apply when converting
+	// to payload unless the payload is at least this size. If this is zero, the
+	// converter will always apply.
+
+	// MinSize is the size the payload must reach in bytes before the converter is
+	// applied when converting to a payload. If the payload in bytes is smaller
+	// than this amount, it is not converted, just the underlying payload is
+	// returned from ToPayload(s).
+	MinSize int
+}
+
+// IODataConverterAlgorithm represents an IO algorithm to perform byte
+// conversion. It is most commonly used for features like compression. See
+// ZlibAlgorithm for an example.
+type IODataConverterAlgorithm struct {
+
+	// Encoding is the unique encoding for this algorithm. This is set on
+	// ToPayload(s) and only applies this converter if it is the same value set
+	// when converting in FromPayload(s). This cannot be empty.
+	Encoding string
+
+	// NewReader instantiates a reader that converts data from the underlying
+	// reader. If the resulting reader is also an io.Closer, it is closed by the
+	// converter. This cannot be nil.
+	NewReader func(io.Reader) (io.Reader, error)
+
+	// NewWriter instantiates a writer that converts data to the underlying
+	// writer. If the resulting writer is also an io.Closer, it is closed by the
+	// converter before the underlying writer's bytes are used. This cannot be
+	// nil.
+	NewWriter func(io.Writer) (io.Writer, error)
+}
+
+// ZlibAlgorithm is an algorithm for zlib compression.
+var ZlibAlgorithm = IODataConverterAlgorithm{
+	Encoding:  "binary/zlib",
+	NewReader: func(r io.Reader) (io.Reader, error) { return zlib.NewReader(r) },
+	NewWriter: func(w io.Writer) (io.Writer, error) { return zlib.NewWriter(w), nil },
+}
+
+// IODataConverter is a DataConverter that wraps another converter and performs
+// reader/writer IO on the results of the other converter's
+// FromPayload(s)/ToPayload(s) operations respectively.
+type IODataConverter struct {
+	underlying DataConverter
+	options    IODataConverterOptions
+}
+
+// NewIODataConverter creates a new IODataConverter wrapping the given
+// underlying data converter.
+func NewIODataConverter(underlying DataConverter, options IODataConverterOptions) (*IODataConverter, error) {
+	if underlying == nil {
+		return nil, fmt.Errorf("underlying converter must be provided")
+	} else if options.Algorithm.Encoding == "" {
+		return nil, fmt.Errorf("missing algorithm encoding")
+	} else if options.Algorithm.NewReader == nil {
+		return nil, fmt.Errorf("missing algorithm NewReader")
+	} else if options.Algorithm.NewWriter == nil {
+		return nil, fmt.Errorf("missing algorithm NewWriter")
+	}
+	return &IODataConverter{underlying: underlying, options: options}, nil
+}
+
+// ToPayload implements DataConverter.ToPayload using the configured algorithm's
+// writer on the result of the underlying ToPayload result.
+func (i *IODataConverter) ToPayload(value interface{}) (*commonpb.Payload, error) {
+	payload, err := i.underlying.ToPayload(value)
+	if payload == nil || err != nil {
+		return payload, err
+	}
+	return i.toPayload(payload)
+}
+
+// ToPayloads implements DataConverter.ToPayloads using the configured
+// algorithm's writer on the result of the underlying ToPayloads result.
+func (i *IODataConverter) ToPayloads(value ...interface{}) (*commonpb.Payloads, error) {
+	payloads, err := i.underlying.ToPayloads(value...)
+	if payloads == nil || err != nil {
+		return payloads, err
+	}
+	newPayloads := &commonpb.Payloads{Payloads: make([]*commonpb.Payload, len(payloads.Payloads))}
+	for idx, payload := range payloads.Payloads {
+		if newPayloads.Payloads[idx], err = i.toPayload(payload); err != nil {
+			return nil, err
+		}
+	}
+	return newPayloads, nil
+}
+
+func (i *IODataConverter) toPayload(payload *commonpb.Payload) (*commonpb.Payload, error) {
+	// If there is a min size and we're under it, do nothing
+	if payload == nil || (i.options.MinSize > 0 && payload.Size() < i.options.MinSize) {
+		return payload, nil
+	}
+	// Marshal and convert
+	b, err := proto.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	b, err = i.write(b)
+	if err != nil {
+		return nil, err
+	}
+	return &common.Payload{
+		Metadata: map[string][]byte{MetadataEncoding: []byte(i.options.Algorithm.Encoding)},
+		Data:     b,
+	}, nil
+}
+
+func (i *IODataConverter) write(b []byte) ([]byte, error) {
+	// Create writer for use
+	// TODO(cretz): Have a reuseable buffer pool?
+	var buf bytes.Buffer
+	w, err := i.options.Algorithm.NewWriter(&buf)
+	if err != nil {
+		return nil, err
+	}
+	// Write the bytes
+	_, err = w.Write(b)
+	// Close no matter what. We choose to close here instead of deferred because
+	// many IO algorithms want the writer closed to flush all writes.
+	if closer, _ := w.(io.Closer); closer != nil {
+		// Only set the error if not already set
+		if closeErr := closer.Close(); closeErr != nil && err == nil {
+			err = closeErr
+		}
+	}
+	return buf.Bytes(), err
+}
+
+// FromPayload implements DataConverter.FromPayload using the configured
+// algorithm's reader before sending to the underlying FromPayload.
+func (i *IODataConverter) FromPayload(payload *commonpb.Payload, valuePtr interface{}) error {
+	payload, err := i.fromPayload(payload)
+	if err != nil {
+		return err
+	}
+	return i.underlying.FromPayload(payload, valuePtr)
+}
+
+// FromPayloads implements DataConverter.FromPayloads using the configured
+// algorithm's reader before sending to the underlying FromPayloads.
+func (i *IODataConverter) FromPayloads(payloads *commonpb.Payloads, valuePtrs ...interface{}) error {
+	if payloads == nil {
+		return i.underlying.FromPayloads(payloads, valuePtrs...)
+	}
+	newPayloads := &commonpb.Payloads{Payloads: make([]*commonpb.Payload, len(payloads.Payloads))}
+	for idx, payload := range payloads.Payloads {
+		var err error
+		if newPayloads.Payloads[idx], err = i.fromPayload(payload); err != nil {
+			return err
+		}
+	}
+	return i.underlying.FromPayloads(newPayloads, valuePtrs...)
+}
+
+func (i *IODataConverter) fromPayload(payload *commonpb.Payload) (*commonpb.Payload, error) {
+	// If the payload is not the expected encoding, do not convert it
+	if payload == nil || string(payload.Metadata[MetadataEncoding]) != i.options.Algorithm.Encoding {
+		return payload, nil
+	}
+	// Convert and unmarshal
+	b, err := i.read(payload.Data)
+	if err != nil {
+		return nil, err
+	}
+	var newPayload commonpb.Payload
+	if err := proto.Unmarshal(b, &newPayload); err != nil {
+		return nil, err
+	}
+	return &newPayload, nil
+}
+
+func (i *IODataConverter) read(b []byte) ([]byte, error) {
+	r, err := i.options.Algorithm.NewReader(bytes.NewReader(b))
+	if err != nil {
+		return nil, err
+	}
+	// Read it all
+	b, err = ioutil.ReadAll(r)
+	// Close reader
+	if closer, _ := r.(io.Closer); closer != nil {
+		// Only set the error if not already set
+		if closeErr := closer.Close(); closeErr != nil && err == nil {
+			err = closeErr
+		}
+	}
+	return b, err
+}
+
+// ToString implements DataConverter.ToString using the configured algorithm's
+// reader before sending to the underlying ToString.
+func (i *IODataConverter) ToString(input *commonpb.Payload) string {
+	input, err := i.fromPayload(input)
+	if err != nil {
+		return err.Error()
+	}
+	return i.underlying.ToString(input)
+}
+
+// ToStrings implements DataConverter.ToStrings using ToString for each value.
+func (i *IODataConverter) ToStrings(input *commonpb.Payloads) []string {
+	if input == nil {
+		return nil
+	}
+	strs := make([]string, len(input.Payloads))
+	for idx, payload := range input.Payloads {
+		strs[idx] = i.ToString(payload)
+	}
+	return strs
+}
---
+++ converter/io_data_converter_test.go
@@ -0,0 +1,122 @@
+package converter_test
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/stretchr/testify/require"
+	"go.temporal.io/sdk/converter"
+)
+
+func ExampleIODataConverter_compression() {
+	defaultConv := converter.GetDefaultDataConverter()
+	// Create Zlib compression converter wrapping the default
+	zlibConv, _ := converter.NewIODataConverter(
+		defaultConv,
+		converter.IODataConverterOptions{Algorithm: converter.ZlibAlgorithm},
+	)
+
+	// Create payloads with both
+	bigString := strings.Repeat("aabbcc", 200)
+	uncompPayload, _ := defaultConv.ToPayload(bigString)
+	compPayload, _ := zlibConv.ToPayload(bigString)
+
+	// The zlib payload is smaller
+	fmt.Printf("Uncompressed payload size: %v (encoding: %s)\n",
+		len(uncompPayload.Data), uncompPayload.Metadata[converter.MetadataEncoding])
+	fmt.Printf("Compressed payload size: %v (encoding: %s)\n",
+		len(compPayload.Data), compPayload.Metadata[converter.MetadataEncoding])
+
+	// Convert from payload and confirm the same string. This uses the same
+	// compression converter because the converter does not do anything to
+	// payloads it didn't previously convert.
+	var uncompValue, compValue string
+	_ = zlibConv.FromPayload(uncompPayload, &uncompValue)
+	_ = zlibConv.FromPayload(compPayload, &compValue)
+	fmt.Printf("Uncompressed payload back to original? %v\n", uncompValue == bigString)
+	fmt.Printf("Compressed payload back to original? %v\n", compValue == bigString)
+
+	// Output:
+	// Uncompressed payload size: 1202 (encoding: json/plain)
+	// Compressed payload size: 57 (encoding: binary/zlib)
+	// Uncompressed payload back to original? true
+	// Compressed payload back to original? true
+}
+
+type SomeStruct struct{ MyValue string }
+
+func TestIODataConverter(t *testing.T) {
+	assertIODataConverter(t, "foo")
+	assertIODataConverter(t, nil)
+	assertIODataConverter(t, []byte("foo"))
+	assertIODataConverter(t, &SomeStruct{MyValue: "somestring"})
+}
+
+func assertIODataConverter(t *testing.T, data interface{}) {
+	defaultConv := converter.GetDefaultDataConverter()
+	zlibConv, err := converter.NewIODataConverter(
+		defaultConv,
+		converter.IODataConverterOptions{Algorithm: converter.ZlibAlgorithm},
+	)
+	require.NoError(t, err)
+
+	// To/FromPayload
+	compPayload, err := zlibConv.ToPayload(data)
+	require.NoError(t, err)
+	require.Equal(t, converter.ZlibAlgorithm.Encoding, string(compPayload.Metadata[converter.MetadataEncoding]))
+	var newData interface{}
+	if data == nil {
+		newData = &newData
+	} else if data != nil {
+		newData = reflect.New(reflect.TypeOf(data)).Interface()
+	}
+	require.NoError(t, zlibConv.FromPayload(compPayload, newData))
+	if data == nil {
+		require.Nil(t, newData)
+	} else {
+		require.Equal(t, data, reflect.ValueOf(newData).Elem().Interface())
+	}
+
+	// To/FromPayloads
+	compPayloads, err := zlibConv.ToPayloads(data)
+	require.NoError(t, err)
+	if data == nil {
+		newData = &newData
+	} else if data != nil {
+		newData = reflect.New(reflect.TypeOf(data)).Interface()
+	}
+	require.NoError(t, zlibConv.FromPayloads(compPayloads, newData))
+	if data == nil {
+		require.Nil(t, newData)
+	} else {
+		require.Equal(t, data, reflect.ValueOf(newData).Elem().Interface())
+	}
+
+	// Ignored if not known encoding
+	uncompPayload, err := defaultConv.ToPayload(data)
+	require.NoError(t, err)
+	if data == nil {
+		newData = &newData
+	} else if data != nil {
+		newData = reflect.New(reflect.TypeOf(data)).Interface()
+	}
+	require.NoError(t, zlibConv.FromPayload(uncompPayload, newData))
+	if data == nil {
+		require.Nil(t, newData)
+	} else {
+		require.Equal(t, data, reflect.ValueOf(newData).Elem().Interface())
+	}
+
+	// Ignored if under the min
+	zlibMaxConv, err := converter.NewIODataConverter(
+		defaultConv,
+		converter.IODataConverterOptions{Algorithm: converter.ZlibAlgorithm, MinSize: 10000},
+	)
+	require.NoError(t, err)
+	compUnderMinPayload, err := zlibMaxConv.ToPayload(data)
+	require.NoError(t, err)
+	require.True(t, proto.Equal(uncompPayload, compUnderMinPayload))
+}
