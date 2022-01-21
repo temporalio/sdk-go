2e27105f0af0ef9ef78dfc48858bba861152b9d0
Chad Retz <chad@temporal.io>
2022-01-21 11:14:59 -0600

    License headers and minor docs

--- contrib/tools/workflowcheck/determinism/checker.go
+++ contrib/tools/workflowcheck/determinism/checker.go
@@ -1,3 +1,25 @@
+// The MIT License
+//
+// Copyright (c) 2022 Temporal Technologies Inc.  All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package determinism
 
 import (
@@ -410,17 +432,20 @@ func (c *collector) applyFuncNonDeterminisms(f *funcInfo, p PackageNonDeterminis
 	}
 }
 
-// PackageLookupCache TODO
+// PackageLookupCache caches fact lookups across packages.
 type PackageLookupCache struct {
 	pass                       *analysis.Pass
 	packageNonDeterminisms     map[*types.Package]PackageNonDeterminisms
 	packageNonDeterminismsLock sync.Mutex
 }
 
+// NewPackageLookupCache creates a PackageLookupCache.
 func NewPackageLookupCache(pass *analysis.Pass) *PackageLookupCache {
 	return &PackageLookupCache{pass: pass, packageNonDeterminisms: map[*types.Package]PackageNonDeterminisms{}}
 }
 
+// PackageNonDeterminisms returns non-determinisms for the package or an empty
+// set if none found.
 func (p *PackageLookupCache) PackageNonDeterminisms(pkg *types.Package) PackageNonDeterminisms {
 	if pkg == nil {
 		return nil
@@ -438,6 +463,10 @@ func (p *PackageLookupCache) PackageNonDeterminisms(pkg *types.Package) PackageN
 	return ret
 }
 
+// PackageNonDeterminismsFromName returns the package for the given name and its
+// non-determinisms via PackageNonDeterminisms. The package name must be
+// directly imported from the given package in scope. Nil is returned for a
+// package that is not found.
 func (p *PackageLookupCache) PackageNonDeterminismsFromName(
 	pkgInScope *types.Package,
 	importedPkg string,
