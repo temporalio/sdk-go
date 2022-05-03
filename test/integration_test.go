7c3777acd5837b62048a905773caf744b68dd3f2
Chad Retz <chad@temporal.io>
2022-05-03 09:33:03 -0500

    Properly set non-determinism as task failure cause (#798)
    Fixes #513

--- test/integration_test.go
+++ test/integration_test.go
@@ -216,7 +216,8 @@ func (ts *IntegrationTestSuite) SetupTest() {
 		}
 	}
 
-	if strings.Contains(ts.T().Name(), "TestSessionOnWorkerFailure") {
+	if strings.Contains(ts.T().Name(), "TestSessionOnWorkerFailure") ||
+		strings.Contains(ts.T().Name(), "TestNonDeterminismFailureCause") {
 		// We disable sticky execution here since we kill the worker and restart it
 		// and sticky execution adds a 5s penalty
 		worker.SetStickyWorkflowCacheSize(0)
@@ -226,7 +227,8 @@ func (ts *IntegrationTestSuite) SetupTest() {
 		options.LocalActivityWorkerOnly = true
 	}
 
-	if strings.Contains(ts.T().Name(), "CancelTimerViaDeferAfterWFTFailure") {
+	if strings.Contains(ts.T().Name(), "CancelTimerViaDeferAfterWFTFailure") ||
+		strings.Contains(ts.T().Name(), "TestNonDeterminismFailureCause") {
 		options.WorkflowPanicPolicy = worker.BlockWorkflow
 	}
 
@@ -2152,6 +2154,68 @@ func (ts *IntegrationTestSuite) TestLargeHistoryReplay() {
 		ts.config.Namespace, workflow.Execution{ID: run.GetID(), RunID: run.GetRunID()})
 	ts.Error(err)
 	ts.Contains(err.Error(), "intentional panic")
+}
+
+func (ts *IntegrationTestSuite) TestNonDeterminismFailureCauseBadStateMachine() {
+	ts.testNonDeterminismFailureCause(false)
+}
+
+func (ts *IntegrationTestSuite) TestNonDeterminismFailureCauseHistoryMismatch() {
+	ts.testNonDeterminismFailureCause(true)
+}
+
+func (ts *IntegrationTestSuite) testNonDeterminismFailureCause(historyMismatch bool) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// Start workflow
+	forcedNonDeterminismCounter = 0
+	run, err := ts.client.ExecuteWorkflow(
+		ctx,
+		ts.startWorkflowOptions("test-non-determinism-failure-cause-"+uuid.New()),
+		ts.workflows.ForcedNonDeterminism,
+		historyMismatch,
+	)
+	ts.NoError(err)
+	defer func() { _ = ts.client.TerminateWorkflow(ctx, run.GetID(), run.GetRunID(), "", nil) }()
+
+	// Wait for tick count as 1, send tick to do an action, then wait for 2
+	ts.waitForQueryTrue(run, "is-wait-tick-count", 1)
+	ts.NoError(ts.client.SignalWorkflow(ctx, run.GetID(), run.GetRunID(), "tick", nil))
+	ts.waitForQueryTrue(run, "is-wait-tick-count", 2)
+
+	// Now, stop the worker and start a new one
+	ts.worker.Stop()
+	ts.workerStopped = true
+	nextWorker := worker.New(ts.client, ts.taskQueueName, worker.Options{DisableStickyExecution: true})
+	ts.registerWorkflowsAndActivities(nextWorker)
+	ts.NoError(nextWorker.Start())
+	defer nextWorker.Stop()
+
+	// Increase the determinism counter and send a tick to trigger replay
+	// non-determinism
+	forcedNonDeterminismCounter++
+	ts.NoError(ts.client.SignalWorkflow(ctx, run.GetID(), run.GetRunID(), "tick", nil))
+
+	// Now let's try to get history until we see a task failure
+	var histErr error
+	var taskFailed *historypb.WorkflowTaskFailedEventAttributes
+	ts.Eventually(func() bool {
+		iter := ts.client.GetWorkflowHistory(
+			ctx, run.GetID(), run.GetRunID(), false, enumspb.HISTORY_EVENT_FILTER_TYPE_ALL_EVENT)
+		for iter.HasNext() {
+			event, err := iter.Next()
+			taskFailed, histErr = event.GetWorkflowTaskFailedEventAttributes(), err
+			if taskFailed != nil || histErr != nil {
+				return true
+			}
+		}
+		return false
+	}, 10*time.Second, 300*time.Millisecond)
+
+	// Check the task has the expected cause
+	ts.NoError(histErr)
+	ts.Equal(enumspb.WORKFLOW_TASK_FAILED_CAUSE_NON_DETERMINISTIC_ERROR, taskFailed.Cause)
 }
 
 func (ts *IntegrationTestSuite) TestClientGetNotFollowingRuns() {
