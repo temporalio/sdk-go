1d6ccb032afae976086af8ab0fd84d88aad083d7
Rob Holland <rob@temporal.io>
2022-12-05 10:26:55 +0000

    Rewrite service proxy and interceptor to use new api-go code.

--- converter/grpc_interceptor_test.go
+++ converter/grpc_interceptor_test.go
@@ -33,11 +33,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	commandpb "go.temporal.io/api/command/v1"
 	commonpb "go.temporal.io/api/common/v1"
-	enumspb "go.temporal.io/api/enums/v1"
-	"go.temporal.io/api/failure/v1"
-	"go.temporal.io/api/history/v1"
 	"go.temporal.io/api/workflowservice/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -60,118 +56,6 @@ func encodedPayloads() *commonpb.Payloads {
 
 func payloadEncoding(payloads *commonpb.Payloads) string {
 	return string(payloads.Payloads[0].Metadata[MetadataEncoding])
-}
-
-func TestServiceInterceptorRequests(t *testing.T) {
-	require := require.New(t)
-
-	s := serviceInterceptor{
-		codecs: []PayloadCodec{NewZlibCodec(ZlibCodecOptions{AlwaysEncode: true})},
-	}
-
-	startReq := &workflowservice.StartWorkflowExecutionRequest{
-		Input: unencodedPayloads(),
-	}
-	err := s.process(true, startReq)
-	require.NoError(err)
-
-	require.Equal("binary/zlib", payloadEncoding(startReq.Input))
-
-	respondWorkflowTaskCompletedReq := &workflowservice.RespondWorkflowTaskCompletedRequest{
-		Commands: []*commandpb.Command{
-			{
-				CommandType: enumspb.COMMAND_TYPE_COMPLETE_WORKFLOW_EXECUTION,
-				Attributes: &commandpb.Command_CompleteWorkflowExecutionCommandAttributes{
-					CompleteWorkflowExecutionCommandAttributes: &commandpb.CompleteWorkflowExecutionCommandAttributes{
-						Result: unencodedPayloads(),
-					},
-				},
-			},
-		},
-	}
-	err = s.process(true, respondWorkflowTaskCompletedReq)
-	require.NoError(err)
-
-	result := respondWorkflowTaskCompletedReq.Commands[0].GetCompleteWorkflowExecutionCommandAttributes().Result
-	require.Equal("binary/zlib", payloadEncoding(result))
-
-	respondActivityTaskFailedReq := &workflowservice.RespondActivityTaskFailedRequest{
-		Failure: &failure.Failure{
-			FailureInfo: &failure.Failure_ApplicationFailureInfo{
-				ApplicationFailureInfo: &failure.ApplicationFailureInfo{
-					Details: unencodedPayloads(),
-				},
-			},
-		},
-	}
-	err = s.process(true, respondActivityTaskFailedReq)
-	require.NoError(err)
-
-	require.Equal("binary/zlib", payloadEncoding(respondActivityTaskFailedReq.Failure.GetApplicationFailureInfo().Details))
-}
-
-func TestServiceInterceptorResponses(t *testing.T) {
-	require := require.New(t)
-
-	s := serviceInterceptor{
-		codecs: []PayloadCodec{NewZlibCodec(ZlibCodecOptions{AlwaysEncode: true})},
-	}
-
-	historyRes := &workflowservice.GetWorkflowExecutionHistoryResponse{
-		History: &history.History{
-			Events: []*history.HistoryEvent{
-				{
-					EventType: enumspb.EVENT_TYPE_WORKFLOW_EXECUTION_STARTED,
-					Attributes: &history.HistoryEvent_WorkflowExecutionStartedEventAttributes{
-						WorkflowExecutionStartedEventAttributes: &history.WorkflowExecutionStartedEventAttributes{
-							Input: encodedPayloads(),
-						},
-					},
-				},
-			},
-		},
-	}
-	err := s.process(false, historyRes)
-	require.NoError(err)
-
-	input := historyRes.History.Events[0].GetWorkflowExecutionStartedEventAttributes().Input
-
-	require.Equal("json/plain", payloadEncoding(input))
-
-	pollWorkflowRes := &workflowservice.PollWorkflowTaskQueueResponse{
-		History: &history.History{
-			Events: []*history.HistoryEvent{
-				{
-					EventType: enumspb.EVENT_TYPE_WORKFLOW_EXECUTION_STARTED,
-					Attributes: &history.HistoryEvent_WorkflowExecutionStartedEventAttributes{
-						WorkflowExecutionStartedEventAttributes: &history.WorkflowExecutionStartedEventAttributes{
-							Input: encodedPayloads(),
-						},
-					},
-				},
-			},
-		},
-	}
-	err = s.process(false, pollWorkflowRes)
-	require.NoError(err)
-
-	input = pollWorkflowRes.History.Events[0].GetWorkflowExecutionStartedEventAttributes().Input
-
-	require.Equal("json/plain", payloadEncoding(input))
-
-	pollActivityRes := &workflowservice.PollActivityTaskQueueResponse{
-		Input:            encodedPayloads(),
-		HeartbeatDetails: encodedPayloads(),
-	}
-	err = s.process(false, pollActivityRes)
-	require.NoError(err)
-
-	require.Equal("json/plain", payloadEncoding(pollActivityRes.Input))
-	require.Equal("json/plain", payloadEncoding(pollActivityRes.HeartbeatDetails))
-
-	emptyPollActivityRes := &workflowservice.PollActivityTaskQueueResponse{}
-	err = s.process(false, emptyPollActivityRes)
-	require.NoError(err)
 }
 
 func TestClientInterceptor(t *testing.T) {
