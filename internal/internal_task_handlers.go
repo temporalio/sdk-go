787cd804777279835487ce5e072390a6afbb6ea6
Quinn Klassen <klassenq@gmail.com>
2023-03-27 08:07:45 -0700

    Track the worker for each workflow context (#1070)
    If a different worker is trying to use a workflow context from a different
    worker discard and create a new context.

--- internal/internal_task_handlers.go
+++ internal/internal_task_handlers.go
@@ -602,20 +602,41 @@ func (wth *workflowTaskHandlerImpl) getOrCreateWorkflowContext(
 	if task.Query == nil || (task.Query != nil && !isFullHistory) {
 		workflowContext = wth.cache.getWorkflowContext(runID)
 	}
-
+	// Verify the cached state is current and for the correct worker
 	if workflowContext != nil {
 		workflowContext.Lock()
-		if task.Query != nil && !isFullHistory {
+		if task.Query != nil && !isFullHistory && wth == workflowContext.wth {
 			// query task and we have a valid cached state
 			metricsHandler.Counter(metrics.StickyCacheHit).Inc(1)
-		} else if history.Events[0].GetEventId() == workflowContext.previousStartedEventID+1 {
+		} else if history.Events[0].GetEventId() == workflowContext.previousStartedEventID+1 && wth == workflowContext.wth {
 			// non query task and we have a valid cached state
 			metricsHandler.Counter(metrics.StickyCacheHit).Inc(1)
 		} else {
-			// non query task and cached state is missing events, we need to discard the cached state and rebuild one.
-			_ = workflowContext.ResetIfStale(task, historyIterator)
+			// possible another task already destroyed this context.
+			if !workflowContext.IsDestroyed() {
+				// non query task and cached state is missing events, we need to discard the cached state and build a new one.
+				if history.Events[0].GetEventId() != workflowContext.previousStartedEventID+1 {
+					wth.logger.Debug("Cached state staled, new task has unexpected events",
+						tagWorkflowID, task.WorkflowExecution.GetWorkflowId(),
+						tagRunID, task.WorkflowExecution.GetRunId(),
+						tagAttempt, task.Attempt,
+						tagCachedPreviousStartedEventID, workflowContext.previousStartedEventID,
+						tagTaskFirstEventID, task.History.Events[0].GetEventId(),
+						tagTaskStartedEventID, task.GetStartedEventId(),
+						tagPreviousStartedEventID, task.GetPreviousStartedEventId(),
+					)
+				} else {
+					wth.logger.Debug("Cached state started on different worker, creating new context")
+				}
+				wth.cache.removeWorkflowContext(runID)
+				workflowContext.clearState()
+			}
+			workflowContext.Unlock(err)
+			workflowContext = nil
 		}
-	} else {
+	}
+	// If the workflow was not cached or the cache was stale.
+	if workflowContext == nil {
 		if !isFullHistory {
 			// we are getting partial history task, but cached state was already evicted.
 			// we need to reset history so we get events from beginning to replay/rebuild the state
