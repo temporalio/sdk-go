3d9a1ef83a29429c01f9d06b794595273634e65d
Jesús García Crespo <jesus@sevein.com>
2023-03-28 14:18:36 +0200

    Fix DevServer tests (#1072)

--- testsuite/devserver_test.go
+++ testsuite/devserver_test.go
@@ -35,7 +35,7 @@ import (
 func TestStartDevServer_Defaults(t *testing.T) {
 	server, err := testsuite.StartDevServer(context.Background(), testsuite.DevServerOptions{})
 	require.NoError(t, err)
-	defer require.NoError(t, server.Stop())
+	defer func() { _ = server.Stop() }()
 	info, err := server.Client().WorkflowService().GetSystemInfo(context.Background(), &workflowservice.GetSystemInfoRequest{})
 	require.NoError(t, err)
 	require.NotNil(t, info.Capabilities)
@@ -44,7 +44,7 @@ func TestStartDevServer_Defaults(t *testing.T) {
 func TestStartDevServer_SpecificVersion(t *testing.T) {
 	server, err := testsuite.StartDevServer(context.Background(), testsuite.DevServerOptions{CachedDownload: testsuite.CachedDownload{Version: "v0.3.0"}})
 	require.NoError(t, err)
-	defer require.NoError(t, server.Stop())
+	defer func() { _ = server.Stop() }()
 	info, err := server.Client().WorkflowService().GetSystemInfo(context.Background(), &workflowservice.GetSystemInfoRequest{})
 	require.NoError(t, err)
 	require.NotNil(t, info.Capabilities)
@@ -53,7 +53,7 @@ func TestStartDevServer_SpecificVersion(t *testing.T) {
 func TestStartDevServer_CustomNamespace(t *testing.T) {
 	server, err := testsuite.StartDevServer(context.Background(), testsuite.DevServerOptions{ClientOptions: &client.Options{Namespace: "testing"}})
 	require.NoError(t, err)
-	defer require.NoError(t, server.Stop())
+	defer func() { _ = server.Stop() }()
 	info, err := server.Client().WorkflowService().DescribeNamespace(context.Background(), &workflowservice.DescribeNamespaceRequest{Namespace: "testing"})
 	require.NoError(t, err)
 	require.Equal(t, "testing", info.NamespaceInfo.Name)
