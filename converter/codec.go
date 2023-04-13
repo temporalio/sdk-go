47507463d9748400f064f43fd032e2449c69bd5a
Matt McShane <matt.mcshane@temporal.io>
2023-04-13 15:57:52 -0400

    Remove orphaned go:generate directives
    The codegen referenced here has been removed from this repository.

--- converter/codec.go
+++ converter/codec.go
@@ -20,8 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-//go:generate go run ../internal/cmd/generateinterceptor/main.go
-
 package converter
 
 import (
