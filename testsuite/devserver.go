f36c24c9d8bf277b6781e1837fc160cb29418495
feedmeapples <aksenov.ro@outlook.com>
2023-06-06 22:42:28 -0400

    Kill dev server on Windows when stopping

--- testsuite/devserver.go
+++ testsuite/devserver.go
@@ -361,6 +361,11 @@ func retryFor(maxAttempts int, interval time.Duration, cond func() error) error 
 
 // Stop the running server and wait for shutdown to complete. Error is propagated from server shutdown.
 func (s *DevServer) Stop() error {
+	if runtime.GOOS == "windows" {
+		// On Windows, we can't send SIGTERM to the process, so we just kill it.
+		return s.cmd.Process.Kill()
+	}
+
 	if err := s.cmd.Process.Signal(syscall.SIGTERM); err != nil {
 		return err
 	}
