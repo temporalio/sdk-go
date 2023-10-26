0d40b872b2e17d8fdfbf9a046c88b9f034290c11
Tim Deeb-Swihart <tim.deebswihart@temporal.io>
2023-10-26 16:36:13 -0700

    Try a new format

--- internal/internal_worker.go
+++ internal/internal_worker.go
@@ -1418,7 +1418,7 @@ func (aw *WorkflowReplayer) replayWorkflowHistory(logger log.Logger, service wor
 			}
 		}
 	}
-	return fmt.Errorf("replay workflow doesn't return the same result as the last event, resp: (%T) %v, last: %v", resp, resp, last)
+	return fmt.Errorf("replay workflow doesn't return the same result as the last event, resp: %s, last: %s", resp, last)
 }
 
 func extractHistoryFromFile(jsonfileName string, lastEventID int64) (*historypb.History, error) {
