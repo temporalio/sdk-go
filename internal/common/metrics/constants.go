d0d7304d0f48ec559ce2e1202fb01f735e8ea01e
Roey Berman <roey.berman@gmail.com>
2024-05-16 17:31:14 -0700

    Group metric defs

--- internal/common/metrics/constants.go
+++ internal/common/metrics/constants.go
@@ -43,16 +43,11 @@ const (
 	WorkflowTaskNoCompletionCounter     = TemporalMetricsPrefix + "workflow_task_no_completion"
 
 	ActivityPollNoTaskCounter             = TemporalMetricsPrefix + "activity_poll_no_task"
-	NexusPollNoTaskCounter                = TemporalMetricsPrefix + "nexus_poll_no_task"
 	ActivityScheduleToStartLatency        = TemporalMetricsPrefix + "activity_schedule_to_start_latency"
 	ActivityExecutionFailedCounter        = TemporalMetricsPrefix + "activity_execution_failed"
-	NexusTaskScheduleToStartLatency       = TemporalMetricsPrefix + "nexus_task_schedule_to_start_latency"
-	NexusTaskExecutionFailedCounter       = TemporalMetricsPrefix + "nexus_task_execution_failed"
 	UnregisteredActivityInvocationCounter = TemporalMetricsPrefix + "unregistered_activity_invocation"
 	ActivityExecutionLatency              = TemporalMetricsPrefix + "activity_execution_latency"
-	NexusTaskExecutionLatency             = TemporalMetricsPrefix + "nexus_task_execution_latency"
 	ActivitySucceedEndToEndLatency        = TemporalMetricsPrefix + "activity_succeed_endtoend_latency"
-	NexusTaskEndToEndLatency              = TemporalMetricsPrefix + "nexus_task_endtoend_latency"
 	ActivityTaskErrorCounter              = TemporalMetricsPrefix + "activity_task_error"
 
 	LocalActivityTotalCounter             = TemporalMetricsPrefix + "local_activity_total"
@@ -86,6 +81,12 @@ const (
 	StickyCacheSize                = TemporalMetricsPrefix + "sticky_cache_size"
 
 	WorkflowActiveThreadCount = TemporalMetricsPrefix + "workflow_active_thread_count"
+
+	NexusPollNoTaskCounter          = TemporalMetricsPrefix + "nexus_poll_no_task"
+	NexusTaskScheduleToStartLatency = TemporalMetricsPrefix + "nexus_task_schedule_to_start_latency"
+	NexusTaskExecutionFailedCounter = TemporalMetricsPrefix + "nexus_task_execution_failed"
+	NexusTaskExecutionLatency       = TemporalMetricsPrefix + "nexus_task_execution_latency"
+	NexusTaskEndToEndLatency        = TemporalMetricsPrefix + "nexus_task_endtoend_latency"
 )
 
 // Metric tag keys
