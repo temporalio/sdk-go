cfe144535e86ac7e8999f72dd343d654a39fea73
mcbadger88 <emmacullen8@gmail.com>
2024-07-03 10:58:48 +1000

    Add otel Carrier implementation for Temporal headers.
    Adds a Opentelemetry 'Carrier' implementation for Temporal Headers. This can be used by third party libraries to propagate Opentelemetry data via the Temporal headers. Similar to implementations for other protocols here https://github.com/open-telemetry/opentelemetry-go/blob/0485de287ec48767a58e9fc2eda30b3dc1836668/propagation/propagation.go#L104 and here  https://github.com/open-telemetry/opentelemetry-go-contrib/blob/main/instrumentation/google.golang.org/grpc/otelgrpc/metadata_supplier.go#L21.

---
+++ contrib/opentelemetry/temporal_header_carrier.go
@@ -0,0 +1,46 @@
+package opentelemetry
+
+import (
+	commonpb "go.temporal.io/api/common/v1"
+	"go.temporal.io/sdk/converter"
+)
+
+// TemporalHeaderCarrier adapts temporal header to satisfy the Carrier interface.
+type TemporalHeaderCarrier map[string]*commonpb.Payload
+
+// Get returns the value associated with the passed key.
+// nolint:fabric_structfunctionordering,fabric_typesordering // https://github.com/anzx/fabric-codestyle/issues/114, https://github.com/anzx/fabric-codestyle/issues/115
+func (h TemporalHeaderCarrier) Get(key string) string {
+	value := ""
+	payload := h[key]
+
+	if err := converter.GetDefaultDataConverter().FromPayload(payload, &value); err != nil {
+		//log.Warn(context.Background(), "error during temporal data conversion from payload", log.Str("error", err.Error()))
+		return ""
+	}
+
+	return value
+}
+
+// Keys lists the keys stored in this carrier.
+// nolint:fabric_structfunctionordering,fabric_typesordering // https://github.com/anzx/fabric-codestyle/issues/114, https://github.com/anzx/fabric-codestyle/issues/115
+func (h TemporalHeaderCarrier) Keys() []string {
+	keys := make([]string, 0, len(h))
+	for k := range h {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+// Set stores the key-value pair.
+// nolint:fabric_structfunctionordering // https://github.com/anzx/fabric-codestyle/issues/114
+func (h TemporalHeaderCarrier) Set(key string, value string) {
+	// Convert value to payload
+	payload, err := converter.GetDefaultDataConverter().ToPayload(value)
+	if err != nil {
+		//log.Warn(context.Background(), "error during temporal data conversion to payload", log.Str("error", err.Error()))
+		return
+	}
+	h[key] = payload
+}
