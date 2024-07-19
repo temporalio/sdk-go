8ea64ae82b0d778e21b68b46fe8837f245b97bc4
Roey Berman <roey.berman@gmail.com>
2024-07-19 16:04:47 -0700

    Skip another flaky versioning test

--- test/worker_versioning_test.go
+++ test/worker_versioning_test.go
@@ -796,6 +796,8 @@ func (ts *WorkerVersioningTestSuite) TestReachabilityVersionsWithRules() {
 }
 
 func (ts *WorkerVersioningTestSuite) TestBuildIDChangesOverWorkflowLifetime() {
+	// TODO: Unskip this test, it is flaky with server 1.25.0-rc.0
+	ts.T().SkipNow()
 	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout)
 	defer cancel()
 
