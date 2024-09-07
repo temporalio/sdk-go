cf9a0eeac2d57cbb20fdd309ee4e7ddc4d6f3579
Dan Davison <dan.davison@temporal.io>
2024-09-07 14:24:40 -0400

    Fix update documentation

--- internal/workflow.go
+++ internal/workflow.go
@@ -1990,8 +1990,8 @@ func (wc *workflowEnvironmentInterceptor) SetQueryHandlerWithOptions(
 // SetUpdateHandler binds an update handler function to the specified
 // name such that update invocations specifying that name will invoke the
 // handler.  The handler function can take as input any number of parameters so
-// long as they can be serialized/deserialized by the system. The handler can
-// take a workflow.Context as its first parameter but this is not required. The
+// long as they can be serialized/deserialized by the system. The handler must
+// take a workflow.Context as its first parameter. The
 // update handler must return either a single error or a single serializable
 // object along with a single error. The update handler function is invoked in
 // the context of the workflow and thus is subject to the same restrictions as
--- workflow/workflow.go
+++ workflow/workflow.go
@@ -534,8 +534,8 @@ func SetUpdateHandler(ctx Context, updateName string, handler interface{}) error
 // SetUpdateHandlerWithOptions binds an update handler function to the specified
 // name such that update invocations specifying that name will invoke the
 // handler.  The handler function can take as input any number of parameters so
-// long as they can be serialized/deserialized by the system. The handler can
-// take a [workflow.Context] as its first parameter but this is not required. The
+// long as they can be serialized/deserialized by the system. The handler must
+// take a [workflow.Context] as its first parameter. The
 // update handler must return either a single error or a single serializable
 // object along with a single error. The update handler function is invoked in
 // the context of the workflow and thus is subject to the same restrictions as
@@ -561,11 +561,11 @@ func SetUpdateHandler(ctx Context, updateName string, handler interface{}) error
 //		err := workflow.SetUpdateHandlerWithOptions(
 //			ctx,
 //			"add",
-//			func(val int) (int, error) { // Calls
+//			func(ctx workflow.Context, val int) (int, error) {
 //				counter += val // note that this mutates workflow state
 //				return counter, nil
 //			},
-//			UpdateHandlerOptions{
+//			workflow.UpdateHandlerOptions{
 //				Validator: func(val int) error {
 //					if val < 0 { // reject attempts to add negative values
 //						return fmt.Errorf("invalid addend: %v", val)
