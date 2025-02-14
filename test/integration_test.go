eaf5d4049fe19b5b0ba2daacf7e3e623c67e5e15
Andrew Yuan <Andrew.Yuan@temporal.io>
2025-02-14 15:36:26 -0800

    Revert "temporarily disable TestNumPollersCounter while I look into why it's flaking (#1826)"
    This reverts commit f22335976ca6a58d75196d464a4ee083bfece3ff.

--- test/integration_test.go
+++ test/integration_test.go
@@ -2897,12 +2897,12 @@ func (ts *IntegrationTestSuite) waitForQueryTrue(run client.WorkflowRun, query s
 }
 
 func (ts *IntegrationTestSuite) TestNumPollersCounter() {
-	_, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	_, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	assertNumPollersEventually := func(expected float64, pollerType string, tags ...string) {
 		// Try for two seconds
 		var lastCount float64
-		for start := time.Now(); time.Since(start) <= 20*time.Second; {
+		for start := time.Now(); time.Since(start) <= 10*time.Second; {
 			lastCount = ts.metricGauge(
 				metrics.NumPoller,
 				"poller_type", pollerType,
@@ -2916,9 +2916,8 @@ func (ts *IntegrationTestSuite) TestNumPollersCounter() {
 		// Will fail
 		ts.Equal(expected, lastCount)
 	}
-	fmt.Println("ts.config.maxWorkflowCacheSize", ts.config.maxWorkflowCacheSize)
 	if ts.config.maxWorkflowCacheSize == 0 {
-		assertNumPollersEventually(2, "workflow_task") // TODO: This is failing
+		assertNumPollersEventually(2, "workflow_task")
 		assertNumPollersEventually(0, "workflow_sticky_task")
 	} else {
 		assertNumPollersEventually(1, "workflow_task")
