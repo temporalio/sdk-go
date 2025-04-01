4c897cd0de70ae042881f303b3e321a70990cfc5
Chad Retz <chad@temporal.io>
2025-04-01 15:21:03 +0100

    Support environment configuration (#1849)
    Fixes #1815

---
+++ contrib/envconfig/client_config_load.go
@@ -0,0 +1,413 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package envconfig
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"go.temporal.io/sdk/client"
+)
+
+// MustLoadDefaultClientOptions invokes [LoadDefaultClientOptions] and panics on error.
+//
+// WARNING: Environment configuration is currently experimental.
+func MustLoadDefaultClientOptions() client.Options {
+	c, err := LoadDefaultClientOptions()
+	if err != nil {
+		panic(err)
+	}
+	return c
+}
+
+// LoadDefaultClientOptions loads client options using default information from config files and environment variables.
+// This just delegates to [LoadClientOptions] with the default options set. See that function and associated options for
+// where/how values are loaded.
+//
+// WARNING: Environment configuration is currently experimental.
+func LoadDefaultClientOptions() (client.Options, error) {
+	return LoadClientOptions(LoadClientOptionsRequest{})
+}
+
+// LoadClientOptionsRequest are options for [LoadClientOptions].
+type LoadClientOptionsRequest struct {
+	// Override the file path to use to load the TOML file for config. Defaults to TEMPORAL_CONFIG_FILE environment
+	// variable or if that is unset/empty, defaults to [os.UserConfigDir]/temporal/temporal.toml. If ConfigFileData is
+	// set, this cannot be set and no file loading from disk occurs. Ignored if DisableFile is true.
+	ConfigFilePath string
+
+	// TOML data to load for config. If set, this overrides any file loading. Cannot be set if ConfigFilePath is set.
+	// Ignored if DisableFile is true.
+	ConfigFileData []byte
+
+	// Specific profile to use after file is loaded. Defaults to TEMPORAL_PROFILE environment variable or if that is
+	// unset/empty, defaults to "default". If either this or the environment variable are set, load will fail if the
+	// profile isn't present in the config. Ignored if DisableFile is true.
+	ConfigFileProfile string
+
+	// If true, will error if there are unrecognized keys.
+	ConfigFileStrict bool
+
+	// If true, will not do any TOML loading from file or data. This and DisableEnv cannot both be true.
+	DisableFile bool
+
+	// If true, will not apply environment variables on top of file config for the client options, but
+	// TEMPORAL_CONFIG_FILE and TEMPORAL_PROFILE environment variables may still by used to populate defaults in this
+	// options structure.
+	DisableEnv bool
+
+	// If true and a codec is configured, the data converter of the client will point to the codec remotely. Users
+	// should usually not set this and rather configure the codec locally. Users should especially not enable this for
+	// clients used by workers since they call the codec repeatedly even during workflow replay.
+	IncludeRemoteCodec bool
+
+	// Override the environment variable lookup. If nil, defaults to [EnvLookupOS].
+	EnvLookup EnvLookup
+}
+
+// LoadClientOptions loads client options from file and then applies environment variable overrides. This will not fail
+// if the config file does not exist. This is effectively a shortcut for [LoadClientConfigProfile] +
+// [ClientConfigProfile.ToClientOptions]. See [LoadClientOptionsRequest] and [ClientConfigProfile] on how files and
+// environment variables are applied.
+//
+// WARNING: Environment configuration is currently experimental.
+func LoadClientOptions(options LoadClientOptionsRequest) (client.Options, error) {
+	// Load profile
+	prof, err := LoadClientConfigProfile(LoadClientConfigProfileOptions{
+		ConfigFilePath:    options.ConfigFilePath,
+		ConfigFileData:    options.ConfigFileData,
+		ConfigFileProfile: options.ConfigFileProfile,
+		ConfigFileStrict:  options.ConfigFileStrict,
+		DisableFile:       options.DisableFile,
+		DisableEnv:        options.DisableEnv,
+		EnvLookup:         options.EnvLookup,
+	})
+	if err != nil {
+		return client.Options{}, err
+	}
+
+	// Convert to client options
+	return prof.ToClientOptions(ToClientOptionsRequest{IncludeRemoteCodec: options.IncludeRemoteCodec})
+}
+
+// [LoadClientConfigOptions] are options for [LoadClientConfig].
+type LoadClientConfigOptions struct {
+	// Override the file path to use to load the TOML file for config. Defaults to TEMPORAL_CONFIG_FILE environment
+	// variable or if that is unset/empty, defaults to [os.UserConfigDir]/temporal/temporal.toml. If ConfigFileData is
+	// set, this cannot be set and no file loading from disk occurs.
+	ConfigFilePath string
+
+	// TOML data to load for config. If set, this overrides any file loading. Cannot be set if ConfigFilePath is set.
+	ConfigFileData []byte
+
+	// If true, will error if there are unrecognized keys.
+	ConfigFileStrict bool
+
+	// Override the environment variable lookup (only used to determine which config file to load). If nil,
+	// defaults to [EnvLookupOS].
+	EnvLookup EnvLookup
+}
+
+// LoadClientConfig loads the client configuration structure from TOML. Does not load values from environment variables
+// (but may use environment variables to get which config file to load). This will not fail if the file does not exist.
+// See [ClientConfig.FromTOML] for details on format.
+//
+// WARNING: Environment configuration is currently experimental.
+func LoadClientConfig(options LoadClientConfigOptions) (ClientConfig, error) {
+	var conf ClientConfig
+	// Get which bytes to load from TOML
+	var data []byte
+	if len(options.ConfigFileData) > 0 {
+		if options.ConfigFilePath != "" {
+			return ClientConfig{}, fmt.Errorf("cannot have data and file path")
+		}
+		data = options.ConfigFileData
+	} else {
+		// Get file name which is either set value, env var, or default path
+		file := options.ConfigFilePath
+		if file == "" {
+			env := options.EnvLookup
+			if env == nil {
+				env = EnvLookupOS
+			}
+			// Unlike env vars for the config values, empty and unset env var
+			// for config file path are both treated as unset
+			file, _ = env.LookupEnv("TEMPORAL_CONFIG_FILE")
+		}
+		if file == "" {
+			var err error
+			if file, err = DefaultConfigFilePath(); err != nil {
+				return ClientConfig{}, err
+			}
+		}
+		// Load file, not exist is ok
+		if b, err := os.ReadFile(file); err == nil {
+			data = b
+		} else if !os.IsNotExist(err) {
+			return ClientConfig{}, fmt.Errorf("failed reading file at %v: %w", file, err)
+		}
+	}
+
+	// Parse data
+	if err := conf.FromTOML(data, ClientConfigFromTOMLOptions{Strict: options.ConfigFileStrict}); err != nil {
+		return ClientConfig{}, fmt.Errorf("failed parsing config: %w", err)
+	}
+	return conf, nil
+}
+
+// LoadClientConfigProfileOptions are options for [LoadClientConfigProfile].
+type LoadClientConfigProfileOptions struct {
+	// Override the file path to use to load the TOML file for config. Defaults to TEMPORAL_CONFIG_FILE environment
+	// variable or if that is unset/empty, defaults to [os.UserConfigDir]/temporal/temporal.toml. If ConfigFileData is
+	// set, this cannot be set and no file loading from disk occurs. Ignored if DisableFile is true.
+	ConfigFilePath string
+
+	// TOML data to load for config. If set, this overrides any file loading. Cannot be set if ConfigFilePath is set.
+	// Ignored if DisableFile is true.
+	ConfigFileData []byte
+
+	// Specific profile to use after file is loaded. Defaults to TEMPORAL_PROFILE environment variable or if that is
+	// unset/empty, defaults to "default". If either this or the environment variable are set, load will fail if the
+	// profile isn't present in the config. Ignored if DisableFile is true.
+	ConfigFileProfile string
+
+	// If true, will error if there are unrecognized keys.
+	ConfigFileStrict bool
+
+	// If true, will not do any TOML loading from file or data. This and DisableEnv cannot both be true.
+	DisableFile bool
+
+	// If true, will not apply environment variables on top of file config for the client options, but
+	// TEMPORAL_CONFIG_FILE and TEMPORAL_PROFILE environment variables may still by used to populate defaults in this
+	// options structure.
+	DisableEnv bool
+
+	// Override the environment variable lookup. If nil, defaults to [EnvLookupOS].
+	EnvLookup EnvLookup
+}
+
+// LoadClientConfigProfile loads a specific client config profile from file and then applies environment variable
+// overrides. This will not fail if the config file does not exist. This is effectively a shortcut for
+// [LoadClientConfig] + [ClientConfigProfile.ApplyEnvVars]. See [LoadClientOptionsRequest] and [ClientConfigProfile] on
+// how files and environment variables are applied.
+//
+// WARNING: Environment configuration is currently experimental.
+func LoadClientConfigProfile(options LoadClientConfigProfileOptions) (ClientConfigProfile, error) {
+	if options.DisableFile && options.DisableEnv {
+		return ClientConfigProfile{}, fmt.Errorf("cannot disable file and env")
+	}
+
+	var prof ClientConfigProfile
+
+	// If file is enabled, load it and find just the profile
+	if !options.DisableFile {
+		// Load
+		conf, err := LoadClientConfig(LoadClientConfigOptions{
+			ConfigFilePath:   options.ConfigFilePath,
+			ConfigFileData:   options.ConfigFileData,
+			ConfigFileStrict: options.ConfigFileStrict,
+			EnvLookup:        options.EnvLookup,
+		})
+		if err != nil {
+			return ClientConfigProfile{}, err
+		}
+		// Find user-set profile or use the default. Only fail if the profile was set and not found (if unset and not
+		// found, that's ok).
+		profile := options.ConfigFileProfile
+		profileUnset := false
+		if profile == "" {
+			env := options.EnvLookup
+			if env == nil {
+				env = EnvLookupOS
+			}
+			// Unlike env vars for the config values, empty and unset env var
+			// for config file path are both treated as unset
+			profile, _ = env.LookupEnv("TEMPORAL_PROFILE")
+		}
+		if profile == "" {
+			profile = DefaultConfigFileProfile
+			profileUnset = true
+		}
+		if profPtr := conf.Profiles[profile]; profPtr != nil {
+			prof = *profPtr
+		} else if !profileUnset {
+			return ClientConfigProfile{}, fmt.Errorf("unable to find profile %v in config data", profile)
+		}
+	}
+
+	// If env is enabled, apply it
+	if !options.DisableEnv {
+		if err := prof.ApplyEnvVars(options.EnvLookup); err != nil {
+			return ClientConfigProfile{}, fmt.Errorf("unable to apply env vars: %w", err)
+		}
+	}
+
+	return prof, nil
+}
+
+// DefaultConfigFileProfile is the default profile used.
+const DefaultConfigFileProfile = "default"
+
+// DefaultConfigFilePath is the default config file path used. It is [os.UserConfigDir]/temporal/temporal.toml.
+//
+// WARNING: Environment configuration is currently experimental.
+func DefaultConfigFilePath() (string, error) {
+	userDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", fmt.Errorf("failed getting user config dir: %w", err)
+	}
+	return filepath.Join(userDir, "temporalio", "temporal.toml"), nil
+}
+
+// EnvLookup abstracts environment variable lookup for [ClientConfigProfile.ApplyEnvVars]. [EnvLookupOS] is the common
+// implementation.
+type EnvLookup interface {
+	// Environ gets all environment variables in the same manner as [os.Environ].
+	Environ() []string
+	// Getenv gets a single environment variable in the same manner as [os.LookupEnv].
+	LookupEnv(string) (string, bool)
+}
+
+type envLookupOS struct{}
+
+// EnvLookupOS implements [EnvLookup] for [os].
+var EnvLookupOS EnvLookup = envLookupOS{}
+
+func (envLookupOS) Environ() []string                   { return os.Environ() }
+func (envLookupOS) LookupEnv(key string) (string, bool) { return os.LookupEnv(key) }
+
+// ApplyEnvVars overwrites any values in the profile with environment variables if the environment variables are set and
+// non-empty. If env lookup is nil, defaults to [EnvLookupOS]
+func (c *ClientConfigProfile) ApplyEnvVars(env EnvLookup) error {
+	if env == nil {
+		env = EnvLookupOS
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_ADDRESS"); ok {
+		c.Address = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_NAMESPACE"); ok {
+		c.Namespace = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_API_KEY"); ok {
+		c.APIKey = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS"); ok {
+		if v, ok := envVarToBool(s); ok {
+			if c.TLS == nil {
+				c.TLS = &ClientConfigTLS{}
+			}
+			c.TLS.Disabled = !v
+		}
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_CLIENT_CERT_PATH"); ok {
+		if c.TLS == nil {
+			c.TLS = &ClientConfigTLS{}
+		}
+		c.TLS.ClientCertPath = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_CLIENT_CERT_DATA"); ok {
+		if c.TLS == nil {
+			c.TLS = &ClientConfigTLS{}
+		}
+		c.TLS.ClientCertData = []byte(s)
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_CLIENT_KEY_PATH"); ok {
+		if c.TLS == nil {
+			c.TLS = &ClientConfigTLS{}
+		}
+		c.TLS.ClientKeyPath = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_CLIENT_KEY_DATA"); ok {
+		if c.TLS == nil {
+			c.TLS = &ClientConfigTLS{}
+		}
+		c.TLS.ClientKeyData = []byte(s)
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_SERVER_CA_CERT_PATH"); ok {
+		if c.TLS == nil {
+			c.TLS = &ClientConfigTLS{}
+		}
+		c.TLS.ServerCACertPath = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_SERVER_CA_CERT_DATA"); ok {
+		if c.TLS == nil {
+			c.TLS = &ClientConfigTLS{}
+		}
+		c.TLS.ServerCACertData = []byte(s)
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_SERVER_NAME"); ok {
+		if c.TLS == nil {
+			c.TLS = &ClientConfigTLS{}
+		}
+		c.TLS.ServerName = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_TLS_DISABLE_HOST_VERIFICATION"); ok {
+		if v, ok := envVarToBool(s); ok {
+			if c.TLS == nil {
+				c.TLS = &ClientConfigTLS{}
+			}
+			c.TLS.DisableHostVerification = v
+		}
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_CODEC_ENDPOINT"); ok {
+		if c.Codec == nil {
+			c.Codec = &ClientConfigCodec{}
+		}
+		c.Codec.Endpoint = s
+	}
+	if s, ok := env.LookupEnv("TEMPORAL_CODEC_AUTH"); ok {
+		if c.Codec == nil {
+			c.Codec = &ClientConfigCodec{}
+		}
+		c.Codec.Auth = s
+	}
+
+	// GRPC meta requires crawling the envs to find
+	for _, v := range env.Environ() {
+		if strings.HasPrefix(v, "TEMPORAL_GRPC_META_") {
+			pieces := strings.SplitN(v, "=", 2)
+			if c.GRPCMeta == nil {
+				c.GRPCMeta = map[string]string{}
+			}
+			// Keys have to be normalized
+			key := NormalizeGRPCMetaKey(strings.TrimPrefix(pieces[0], "TEMPORAL_GRPC_META_"))
+			// Empty env vars are not the same as unset. Unset will leave the
+			// meta key unchanged, but empty removes it.
+			if pieces[1] == "" {
+				delete(c.GRPCMeta, key)
+			} else {
+				c.GRPCMeta[key] = pieces[1]
+			}
+		}
+	}
+	return nil
+}
+
+func envVarToBool(val string) (v bool, ok bool) {
+	val = strings.ToLower(val)
+	return val == "1" || val == "true", val == "1" || val == "0" || val == "true" || val == "false"
+}
---
+++ contrib/envconfig/client_config_load_test.go
@@ -0,0 +1,196 @@
+// The MIT License
+//
+// Copyright (c) 2020 Temporal Technologies Inc.  All rights reserved.
+//
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package envconfig_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.temporal.io/sdk/contrib/envconfig"
+)
+
+func TestLoadClientOptionsFile(t *testing.T) {
+	// Put some data in temp file and set the env var to use that file
+	f, err := os.CreateTemp("", "")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+	_, err = f.Write([]byte(`
+[profile.default]
+address = "my-address"
+namespace = "my-namespace"`))
+	f.Close()
+	require.NoError(t, err)
+
+	// Explicitly set
+	opts, err := envconfig.LoadClientOptions(envconfig.LoadClientOptionsRequest{
+		ConfigFilePath: f.Name(),
+		EnvLookup:      EnvLookupMap{},
+	})
+	require.NoError(t, err)
+	require.Equal(t, "my-address", opts.HostPort)
+	require.Equal(t, "my-namespace", opts.Namespace)
+
+	// From env
+	opts, err = envconfig.LoadClientOptions(envconfig.LoadClientOptionsRequest{
+		EnvLookup: EnvLookupMap{"TEMPORAL_CONFIG_FILE": f.Name()},
+	})
+	require.NoError(t, err)
+	require.Equal(t, "my-address", opts.HostPort)
+	require.Equal(t, "my-namespace", opts.Namespace)
+}
+
+func TestLoadClientOptionsAPIKeyTLS(t *testing.T) {
+	// Since API key is present, TLS defaults to present
+	opts, err := envconfig.LoadClientOptions(envconfig.LoadClientOptionsRequest{
+		ConfigFileData: []byte(`
+		[profile.default]
+		api_key = "my-api-key"`),
+		EnvLookup: EnvLookupMap{},
+	})
+	require.NoError(t, err)
+	require.NotNil(t, opts.Credentials)
+	require.NotNil(t, opts.ConnectionOptions.TLS)
+
+	// But when API key is not present, neither should TLS be
+	opts, err = envconfig.LoadClientOptions(envconfig.LoadClientOptionsRequest{
+		ConfigFileData: []byte(`
+		[profile.default]
+		address = "whatever"`),
+		EnvLookup: EnvLookupMap{},
+	})
+	require.NoError(t, err)
+	require.Nil(t, opts.Credentials)
+	require.Nil(t, opts.ConnectionOptions.TLS)
+}
+
+func TestClientProfileApplyEnvVars(t *testing.T) {
+	data := `
+[profile.foo]
+address = "my-address"
+namespace = "my-namespace"
+api_key = "my-api-key"
+codec = { endpoint = "my-endpoint", auth = "my-auth" }
+grpc_meta = { some-heAder1 = "some-value1", some-header2 = "some-value2", some_heaDer3 = "some-value3" }
+some_future_key = "some future value not handled"
+
+[profile.foo.tls]
+disabled = true
+client_cert_path = "my-client-cert-path"
+client_cert_data = "my-client-cert-data"
+client_key_path = "my-client-key-path"
+client_key_data = "my-client-key-data"
+server_ca_cert_path = "my-server-ca-cert-path"
+server_ca_cert_data = "my-server-ca-cert-data"
+server_name = "my-server-name"
+disable_host_verification = true`
+	// No env
+	prof, err := envconfig.LoadClientConfigProfile(envconfig.LoadClientConfigProfileOptions{
+		ConfigFileData:    []byte(data),
+		ConfigFileProfile: "foo",
+		EnvLookup:         EnvLookupMap{},
+	})
+	require.NoError(t, err)
+	require.Equal(t, "my-address", prof.Address)
+	require.Equal(t, "my-namespace", prof.Namespace)
+	require.Equal(t, "my-api-key", prof.APIKey)
+	require.Equal(t, "my-endpoint", prof.Codec.Endpoint)
+	require.Equal(t, "my-auth", prof.Codec.Auth)
+	require.True(t, prof.TLS.Disabled)
+	require.Equal(t, "my-client-cert-path", prof.TLS.ClientCertPath)
+	require.Equal(t, []byte("my-client-cert-data"), prof.TLS.ClientCertData)
+	require.Equal(t, "my-client-key-path", prof.TLS.ClientKeyPath)
+	require.Equal(t, []byte("my-client-key-data"), prof.TLS.ClientKeyData)
+	require.Equal(t, "my-server-ca-cert-path", prof.TLS.ServerCACertPath)
+	require.Equal(t, []byte("my-server-ca-cert-data"), prof.TLS.ServerCACertData)
+	require.Equal(t, "my-server-name", prof.TLS.ServerName)
+	require.True(t, prof.TLS.DisableHostVerification)
+	require.Equal(t, map[string]string{
+		"some-header1": "some-value1",
+		"some-header2": "some-value2",
+		"some-header3": "some-value3",
+	}, prof.GRPCMeta)
+
+	// With env
+	prof, err = envconfig.LoadClientConfigProfile(envconfig.LoadClientConfigProfileOptions{
+		ConfigFileData: []byte(data),
+		EnvLookup: EnvLookupMap{
+			"TEMPORAL_PROFILE":                       "foo",
+			"TEMPORAL_ADDRESS":                       "my-address-new",
+			"TEMPORAL_NAMESPACE":                     "my-namespace-new",
+			"TEMPORAL_API_KEY":                       "my-api-key-new",
+			"TEMPORAL_TLS":                           "true",
+			"TEMPORAL_TLS_CLIENT_CERT_PATH":          "my-client-cert-path-new",
+			"TEMPORAL_TLS_CLIENT_CERT_DATA":          "my-client-cert-data-new",
+			"TEMPORAL_TLS_CLIENT_KEY_PATH":           "my-client-key-path-new",
+			"TEMPORAL_TLS_CLIENT_KEY_DATA":           "my-client-key-data-new",
+			"TEMPORAL_TLS_SERVER_CA_CERT_PATH":       "my-server-ca-cert-path-new",
+			"TEMPORAL_TLS_SERVER_CA_CERT_DATA":       "my-server-ca-cert-data-new",
+			"TEMPORAL_TLS_SERVER_NAME":               "my-server-name-new",
+			"TEMPORAL_TLS_DISABLE_HOST_VERIFICATION": "false",
+			"TEMPORAL_CODEC_ENDPOINT":                "my-endpoint-new",
+			"TEMPORAL_CODEC_AUTH":                    "my-auth-new",
+			// Leave first header alone, replace second header, set third as empty, add a fourth
+			"TEMPORAL_GRPC_META_SOME_HEADER2": "some-value2-new",
+			"TEMPORAL_GRPC_META_SOME_HEADER3": "",
+			"TEMPORAL_GRPC_META_SOME_HEADER4": "some-value4-new",
+		},
+	})
+	require.NoError(t, err)
+	require.Equal(t, "my-address-new", prof.Address)
+	require.Equal(t, "my-namespace-new", prof.Namespace)
+	require.Equal(t, "my-api-key-new", prof.APIKey)
+	require.Equal(t, "my-endpoint-new", prof.Codec.Endpoint)
+	require.Equal(t, "my-auth-new", prof.Codec.Auth)
+	require.False(t, prof.TLS.Disabled)
+	require.Equal(t, "my-client-cert-path-new", prof.TLS.ClientCertPath)
+	require.Equal(t, []byte("my-client-cert-data-new"), prof.TLS.ClientCertData)
+	require.Equal(t, "my-client-key-path-new", prof.TLS.ClientKeyPath)
+	require.Equal(t, []byte("my-client-key-data-new"), prof.TLS.ClientKeyData)
+	require.Equal(t, "my-server-ca-cert-path-new", prof.TLS.ServerCACertPath)
+	require.Equal(t, []byte("my-server-ca-cert-data-new"), prof.TLS.ServerCACertData)
+	require.Equal(t, "my-server-name-new", prof.TLS.ServerName)
+	require.False(t, prof.TLS.DisableHostVerification)
+	require.Equal(t, map[string]string{
+		"some-header1": "some-value1",
+		"some-header2": "some-value2-new",
+		"some-header4": "some-value4-new",
+	}, prof.GRPCMeta)
+}
+
+type EnvLookupMap map[string]string
+
+func (e EnvLookupMap) Environ() []string {
+	ret := make([]string, 0, len(e))
+	for k, v := range e {
+		ret = append(ret, k+"="+v)
+	}
+	return ret
+}
+
+func (e EnvLookupMap) LookupEnv(key string) (string, bool) {
+	v, ok := e[key]
+	return v, ok
+}
