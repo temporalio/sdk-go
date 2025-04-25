4b3e520405f554376a85ae71811a8675896e946d
Thomas Hardy <thomas.hardy@temporal.io>
2025-04-25 12:25:28 -0700

    update comment

--- temporal/error.go
+++ temporal/error.go
@@ -275,7 +275,7 @@ func NewHeartbeatTimeoutError(details ...interface{}) error {
 }
 
 // ApplicationErrorCategory sets the category of the error. The category of the error
-// maps to logging/metrics behaviours.
+// maps to logging/metrics SDK behaviours, does not impact server-side logging/metrics.
 type ApplicationErrorCategory = internal.ApplicationErrorCategory
 
 const (
