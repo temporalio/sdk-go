4fd1686f0e106372963f8fff55739474563b39a5
Andrew Yuan <Andrew.Yuan@temporal.io>
2026-02-05 19:25:11 -0800

    Fix imports

--- contrib/resourcetuner/cgroups_test.go
+++ contrib/resourcetuner/cgroups_test.go
@@ -3,8 +3,12 @@
 package resourcetuner
 
 import (
-	"github.com/stretchr/testify/assert"
+	"errors"
+	"io/fs"
 	"testing"
+
+	"github.com/containerd/cgroups/v3/cgroup2"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestCGroupInfoUpdateOutsideContainer verifies that Update() gracefully handles
@@ -14,9 +18,9 @@ func TestCGroupInfoUpdateOutsideContainer(t *testing.T) {
 	info := newCGroupInfo().(*cGroupInfoImpl)
 	continueUpdates, err := info.Update()
 
-	_, err := cgroup2.Load("/")
+	_, loadErr := cgroup2.Load("/")
 
-	if errors.Is(err, fs.ErrNotExist) {
+	if errors.Is(loadErr, fs.ErrNotExist) {
 		assert.False(t, continueUpdates, "should return false when cgroup files don't exist")
 		assert.NoError(t, err, "should not return error when cgroup files don't exist")
 	}
