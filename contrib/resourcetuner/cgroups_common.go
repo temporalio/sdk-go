ad29c1edef3b315f5f1f310ae15a1fa477cbcfbf
Andrew Yuan <Andrew.Yuan@temporal.io>
2026-02-11 14:58:17 -0800

    ensure it fails

--- contrib/resourcetuner/cgroups_common.go
+++ contrib/resourcetuner/cgroups_common.go
@@ -9,7 +9,7 @@ import (
 // Returns (false, nil) if cgroup files don't exist (not in a container),
 // (true, err) for real errors, and (true, nil) on success.
 func handleCGroupUpdateError(err error) (bool, error) {
-	if errors.Is(err, fs.ErrNotExist) {
+	if !errors.Is(err, fs.ErrNotExist) {
 		return false, nil
 	} else if err != nil {
 		return true, err
