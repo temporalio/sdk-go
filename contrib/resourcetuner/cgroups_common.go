efbe53f6247023120bcbe69a6258682e6d1c0d2c
Andrew Yuan <andrew.yuan@temporal.io>
2026-02-12 16:51:13 -0800

    Fix cgroup detection condition (#2179)
    * Fix condition and add test
    
    * Fix imports
    
    * Use %v for errors
    
    * Organize code so fix can be tested properly
    
    * Fix issing newline

---
+++ contrib/resourcetuner/cgroups_common.go
@@ -0,0 +1,18 @@
+package resourcetuner
+
+import (
+	"errors"
+	"io/fs"
+)
+
+// handleCGroupUpdateError interprets the error from a cgroup stats update.
+// Returns (false, nil) if cgroup files don't exist (not in a container),
+// (true, err) for real errors, and (true, nil) on success.
+func handleCGroupUpdateError(err error) (bool, error) {
+	if errors.Is(err, fs.ErrNotExist) {
+		return false, nil
+	} else if err != nil {
+		return true, err
+	}
+	return true, nil
+}
