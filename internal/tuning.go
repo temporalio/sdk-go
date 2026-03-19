06004323c44d8ae43aa062d3794db2c60c76160a
Andrew Yuan <Andrew.Yuan@temporal.io>
2026-03-19 10:54:19 -0700

    fully go back

--- internal/tuning.go
+++ internal/tuning.go
@@ -322,9 +322,10 @@ func (s slotReserveInfoImpl) WorkerIdentity() string {
 }
 
 func (s slotReserveInfoImpl) NumIssuedSlots() int {
-	slots := s.issuedSlots.Load()
-	//fmt.Println("slots", slots)
-	return int(slots)
+	//slots := s.issuedSlots.Load()
+	////fmt.Println("slots", slots)
+	//return int(slots)
+	return int(s.issuedSlots.Load())
 }
 
 func (s slotReserveInfoImpl) Logger() log.Logger {
