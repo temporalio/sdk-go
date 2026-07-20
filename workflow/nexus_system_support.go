c6e9fc44426f07acbce9758fe4e18378ae5eaa82
Dan Plyukhin <dan.plyukhin@temporal.io>
2026-07-20 11:10:10 -0400

    Removed temporal_model_converters copying to fix go vet

--- workflow/nexus_system_support.go
+++ workflow/nexus_system_support.go
@@ -25,14 +25,6 @@ func durationToProto(_ Context, d *time.Duration) (*durationpb.Duration, error) 
 	return durationpb.New(*d), nil
 }
 
-func durationFromProto(_ Context, d *durationpb.Duration) (*time.Duration, error) {
-	if d == nil {
-		return nil, nil
-	}
-	value := d.AsDuration()
-	return &value, nil
-}
-
 // --- TaskQueue (temporal.api.taskqueue.v1.TaskQueue) ---
 
 func taskQueueToProto(_ Context, name *string) (*taskqueue.TaskQueue, error) {
@@ -40,14 +32,6 @@ func taskQueueToProto(_ Context, name *string) (*taskqueue.TaskQueue, error) {
 		return nil, nil
 	}
 	return &taskqueue.TaskQueue{Name: *name}, nil
-}
-
-func taskQueueFromProto(_ Context, tq *taskqueue.TaskQueue) (*string, error) {
-	if tq == nil {
-		return nil, nil
-	}
-	value := tq.GetName()
-	return &value, nil
 }
 
 // --- RetryPolicy (temporal.api.common.v1.RetryPolicy) ---
@@ -70,24 +54,6 @@ func retryPolicyToProto(_ Context, p *temporal.RetryPolicy) (*common.RetryPolicy
 	return proto, nil
 }
 
-func retryPolicyFromProto(_ Context, p *common.RetryPolicy) (*temporal.RetryPolicy, error) {
-	if p == nil {
-		return nil, nil
-	}
-	policy := temporal.RetryPolicy{
-		BackoffCoefficient:     p.GetBackoffCoefficient(),
-		MaximumAttempts:        p.GetMaximumAttempts(),
-		NonRetryableErrorTypes: p.GetNonRetryableErrorTypes(),
-	}
-	if interval := p.GetInitialInterval(); interval != nil {
-		policy.InitialInterval = interval.AsDuration()
-	}
-	if interval := p.GetMaximumInterval(); interval != nil {
-		policy.MaximumInterval = interval.AsDuration()
-	}
-	return &policy, nil
-}
-
 // --- Priority (temporal.api.common.v1.Priority) ---
 
 func priorityToProto(_ Context, p *temporal.Priority) (*common.Priority, error) {
@@ -101,17 +67,6 @@ func priorityToProto(_ Context, p *temporal.Priority) (*common.Priority, error) 
 	}, nil
 }
 
-func priorityFromProto(_ Context, p *common.Priority) (*temporal.Priority, error) {
-	if p == nil {
-		return nil, nil
-	}
-	return &temporal.Priority{
-		PriorityKey:    int(p.GetPriorityKey()),
-		FairnessKey:    p.GetFairnessKey(),
-		FairnessWeight: p.GetFairnessWeight(),
-	}, nil
-}
-
 // --- WorkflowType (temporal.api.common.v1.WorkflowType) ---
 
 func workflowTypeToProto(_ Context, name *string) (*common.WorkflowType, error) {
@@ -119,14 +74,6 @@ func workflowTypeToProto(_ Context, name *string) (*common.WorkflowType, error) 
 		return nil, nil
 	}
 	return &common.WorkflowType{Name: *name}, nil
-}
-
-func workflowTypeFromProto(_ Context, t *common.WorkflowType) (*string, error) {
-	if t == nil {
-		return nil, nil
-	}
-	value := t.GetName()
-	return &value, nil
 }
 
 // --- Payload / Payloads (temporal.api.common.v1.Payload[s]) ---
@@ -147,21 +94,6 @@ func payloadFromProto(ctx Context, payload *common.Payload) (any, error) {
 
 func payloadsToProto(ctx Context, values []any) (*common.Payloads, error) {
 	return getWorkflowDataConverter(ctx).ToPayloads(values...)
-}
-
-func payloadsFromProto(ctx Context, payloads *common.Payloads) ([]any, error) {
-	if payloads == nil {
-		return nil, nil
-	}
-	values := make([]any, 0, len(payloads.GetPayloads()))
-	for _, payload := range payloads.GetPayloads() {
-		value, err := payloadFromProto(ctx, payload)
-		if err != nil {
-			return nil, err
-		}
-		values = append(values, value)
-	}
-	return values, nil
 }
 
 func getWorkflowDataConverter(ctx Context) converter.DataConverter {
@@ -201,21 +133,6 @@ func memoToProto(ctx Context, memo map[string]any) (*common.Memo, error) {
 		fields[key] = payload
 	}
 	return &common.Memo{Fields: fields}, nil
-}
-
-func memoFromProto(ctx Context, memo *common.Memo) (map[string]any, error) {
-	if memo == nil {
-		return nil, nil
-	}
-	result := make(map[string]any, len(memo.GetFields()))
-	for key, payload := range memo.GetFields() {
-		value, err := payloadFromProto(ctx, payload)
-		if err != nil {
-			return nil, err
-		}
-		result[key] = value
-	}
-	return result, nil
 }
 
 // --- SearchAttributes (temporal.api.common.v1.SearchAttributes) ---
