0feb5a6813ab94b6e790260521a94914bb5e3267
Andrew Yuan <andrew.yuan@temporal.io>
2026-08-14 10:41:37 -0700

    Use synctest for tests (#2548)
    * use synctest
    
    * switch more tests to synctest
    
    * isolate scenario into each own bubble
    
    * simplify test

--- internal/internal_coroutines_test.go
+++ internal/internal_coroutines_test.go
@@ -7,8 +7,8 @@ import (
 	"strconv"
 	"strings"
 	"sync/atomic"
-
 	"testing"
+	"testing/synctest"
 	"time"
 
 	"github.com/stretchr/testify/assert"
@@ -52,23 +52,26 @@ func TestDispatcher(t *testing.T) {
 }
 
 func TestDispatcherDeferClose(t *testing.T) {
-	var value atomic.Bool
-	d := createNewDispatcher(func(ctx Context) {
-		// Block all coroutines on this channel
-		c1 := NewChannel(ctx)
-		defer func() {
-			value.Store(true)
-		}()
-		c1.Receive(ctx, nil)
+	synctest.Test(t, func(t *testing.T) {
+		var value atomic.Bool
+		d := createNewDispatcher(func(ctx Context) {
+			// Block all coroutines on this channel
+			c1 := NewChannel(ctx)
+			defer func() {
+				value.Store(true)
+			}()
+			c1.Receive(ctx, nil)
+		})
+		defer d.Close()
+		require.Equal(t, false, value.Load())
+		requireNoExecuteErr(t, d.ExecuteUntilAllBlocked(defaultDeadlockDetectionTimeout))
+		// Closing the dispatcher will cause the blocked goroutine to stop executing, but defers
+		// will still run.
+		d.Close()
+		require.True(t, d.IsClosed())
+		synctest.Wait()
+		require.True(t, value.Load())
 	})
-	defer d.Close()
-	require.Equal(t, false, value.Load())
-	requireNoExecuteErr(t, d.ExecuteUntilAllBlocked(defaultDeadlockDetectionTimeout))
-	// Closing the dispatcher will cause the blocked goroutine to stop executing, but defers
-	// will still run.
-	d.Close()
-	require.True(t, d.IsClosed())
-	require.Eventually(t, value.Load, time.Second, 10*time.Millisecond)
 }
 
 func TestDispatcherDeadlockedDefer(t *testing.T) {
@@ -93,34 +96,35 @@ func TestDispatcherDeadlockedDefer(t *testing.T) {
 }
 
 func TestDispatcherDeferCloseRace(t *testing.T) {
-	var value atomic.Int32
-	var d dispatcher
-	d = createNewDispatcher(func(ctx Context) {
-		// Block all coroutines on this channel
-		c1 := NewChannel(ctx)
-		for i := 0; i < 100; i++ {
-			index := i
-			id := "coroutine_" + strconv.Itoa(index)
-			d.NewCoroutine(ctx, id, false, func(ctx Context) {
-				defer func() {
-					value.Store(int32(index))
-				}()
-				c1.Receive(ctx, nil)
-			})
-		}
-		c1.Receive(ctx, nil)
-	})
-	defer d.Close()
+	synctest.Test(t, func(t *testing.T) {
+		var value atomic.Int32
+		var d dispatcher
+		d = createNewDispatcher(func(ctx Context) {
+			// Block all coroutines on this channel
+			c1 := NewChannel(ctx)
+			for i := 0; i < 100; i++ {
+				index := i
+				id := "coroutine_" + strconv.Itoa(index)
+				d.NewCoroutine(ctx, id, false, func(ctx Context) {
+					defer func() {
+						value.Store(int32(index))
+					}()
+					c1.Receive(ctx, nil)
+				})
+			}
+			c1.Receive(ctx, nil)
+		})
+		defer d.Close()
 
-	require.Equal(t, int32(0), value.Load())
-	requireNoExecuteErr(t, d.ExecuteUntilAllBlocked(defaultDeadlockDetectionTimeout))
-	// Closing the dispatcher will cause the blocked coroutine to stop executing, but defers
-	// will still run.
-	d.Close()
-	require.True(t, d.IsClosed())
-	require.Eventually(t, func() bool {
-		return value.Load() == int32(99)
-	}, time.Second, 10*time.Millisecond)
+		require.Equal(t, int32(0), value.Load())
+		requireNoExecuteErr(t, d.ExecuteUntilAllBlocked(defaultDeadlockDetectionTimeout))
+		// Closing the dispatcher will cause the blocked coroutine to stop executing, but defers
+		// will still run.
+		d.Close()
+		require.True(t, d.IsClosed())
+		synctest.Wait()
+		require.Equal(t, int32(99), value.Load())
+	})
 }
 
 func TestNonBlockingChildren(t *testing.T) {
