08a0e06f7d3fb436a6c8c9e40a8b0ee904af6146
Dan Plyukhin <dan.plyukhin@temporal.io>
2026-09-12 15:00:11 -0500

    State invariants about transfer type use and panic when violated

--- internal/transfer_types.go
+++ internal/transfer_types.go
@@ -3,7 +3,6 @@ package internal
 import (
 	"context"
 	"fmt"
-	"reflect"
 
 	commonpb "go.temporal.io/api/common/v1"
 	"go.temporal.io/sdk/converter"
@@ -28,6 +27,11 @@ type ValueWithTransferConverter interface {
 
 // TransferConverter converts application values to serializable transfer
 // values and back. Implement it using [NewTransferConverter].
+//
+// The SDK will only invoke tc.ToTransferValue(v) if v.TransferConverter()
+// equals tc. Likewise, the SDK will only invoke tc.FromTransferValue(tv, v)
+// if v.TransferConverter() equals tc and tv was obtained from
+// tc.NewTransferValuePtr().
 //
 // NOTE: Experimental.
 type TransferConverter interface {
@@ -59,37 +63,35 @@ type transferConverter[Value, TransferValue any] struct {
 func (*transferConverter[Value, TransferValue]) transferConverter() {}
 
 func (*transferConverter[Value, TransferValue]) NewTransferValuePtr() any {
-	transferType := reflect.TypeFor[TransferValue]()
-	if transferType.Kind() != reflect.Pointer {
-		return new(TransferValue)
-	}
-
-	transferValue := reflect.New(transferType.Elem())
-	if transferValue.Type() != transferType {
-		transferValue = transferValue.Convert(transferType)
-	}
-	return transferValue.Interface()
+	return new(TransferValue)
 }
 
-func (c *transferConverter[Value, TransferValue]) ToTransferValue(value any) (any, error) {
+func (tc *transferConverter[Value, TransferValue]) ToTransferValue(value any) (any, error) {
 	if value, ok := value.(Value); ok {
-		return c.toTransferValue(value)
+		return tc.toTransferValue(value)
 	}
-	return value, nil
+	// The SDK should only call ToTransferValue on v if v.TransferConverter() equals c.
+	// If we got here, we violated that contract.
+	panic(fmt.Sprintf("Expected type %T, got %T", *new(Value), value))
 }
 
-func (c *transferConverter[Value, TransferValue]) FromTransferValue(transferValue any, valuePtr any) error {
+func (tc *transferConverter[Value, TransferValue]) FromTransferValue(transferValue any, valuePtr any) error {
 	v, ok := valuePtr.(*Value)
 	if !ok {
+		// The SDK should only call FromTransferValue on v if v.TransferConverter() equals c.
+		// If we got here, we violated that contract.
 		var expectedValue *Value
-		return fmt.Errorf("Expected type %T, got %T", expectedValue, valuePtr)
+		panic(fmt.Errorf("Expected type %T, got %T", expectedValue, valuePtr))
 	}
 	t, ok := transferValue.(TransferValue)
 	if !ok {
+		// The SDK should only call FromTransferValue on tv if tv was obtained from
+		// tc.NewTransferValuePtr().
+		// If we got here, we violated that contract.
 		var expectedTransfer TransferValue
-		return fmt.Errorf("Expected transfer type %T, got %T", expectedTransfer, transferValue)
+		panic(fmt.Errorf("Expected transfer type %T, got %T", expectedTransfer, transferValue))
 	}
-	return c.fromTransferValue(t, v)
+	return tc.fromTransferValue(t, v)
 }
 
 // -- DATA CONVERTERS ----------------------------------------------------------
@@ -121,7 +123,7 @@ func makeTransferAware(dc converter.DataConverter) converter.DataConverter {
 }
 
 func (dc *transferAwareDataConverter) ToPayload(value any) (*commonpb.Payload, error) {
-	transferValue, err := toTransferValue(value)
+	transferValue, err := encodeAsTransferValueOrReturn(value)
 	if err != nil {
 		return nil, err
 	}
@@ -130,7 +132,7 @@ func (dc *transferAwareDataConverter) ToPayload(value any) (*commonpb.Payload, e
 
 func (dc *transferAwareDataConverter) ToPayloads(values ...any) (*commonpb.Payloads, error) {
 	for i, value := range values {
-		transferValue, err := toTransferValue(value)
+		transferValue, err := encodeAsTransferValueOrReturn(value)
 		if err != nil {
 			return nil, err
 		}
@@ -139,7 +141,7 @@ func (dc *transferAwareDataConverter) ToPayloads(values ...any) (*commonpb.Paylo
 	return dc.parent.ToPayloads(values...)
 }
 
-func toTransferValue(value any) (transferValue any, err error) {
+func encodeAsTransferValueOrReturn(value any) (transferValue any, err error) {
 	convertible, ok := value.(ValueWithTransferConverter)
 	if !ok {
 		return value, nil
